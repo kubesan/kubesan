kubesan/kubesan

commit 8fddf3f73e48fdbb2d9e4819ae9c832aaf3414ef

containerfile best-practices updates
We are updating a few things in the containerfile:
- Adding in TARGETOS TARGETARCH and TARGETPLATFORM args
  - These allow us to cross-compile and do multiarch builds
- Switched the builder image name from 'makefile' to the more standard 'builder'
- Switched to more generic base images which allows users to pull from
  their preferred repository
- Added the GOOS and GOARCH environment variables to the go build
- Added the platform flag to the fedora base image for multiarch builds
- Added dnf update to make sure all security patches are in the base image
- Added --nodocs and --noplugins for a slimmed down dnf install

Signed-off-by: Jeff Roche <jeroche@redhat.com>
[eblake: also fix a stale comment, hoist vendoring change to earlier patch,
drop attempt to use non-root user for a later patch]
Signed-off-by: Eric Blake <eblake@redhat.com>

Containerfile
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
-
-FROM quay.io/projectquay/golang:1.22 AS makefile
-
-RUN dnf install -qy diffutils && dnf clean all
+ARG TARGETOS
+ARG TARGETARCH
+ARG TARGETPLATFORM
+FROM golang:1.22 AS builder
 
 WORKDIR /kubesan
 
@@ -28,18 +28,24 @@ COPY deploy/ deploy/
 COPY hack/ hack/
 COPY internal/ internal/
 
-RUN make build
+# We set GOOS and GOARCH so go cross-compiles to the correct os and arch
+RUN GOOS=$TARGETOS GOARCH=$TARGETARCH make build
 
 # CentOS Stream 9 doesn't provide package nbd
 # FROM quay.io/centos/centos:stream9
-FROM quay.io/fedora/fedora:40
+# We use --platform=$TARGETPLATFORM to pull the correct arch for
+# the base image. This is needed for multi-arch builds
+FROM --platform=$TARGETPLATFORM fedora:40
 
 # util-linux-core, e2fsprogs, and xfsprogs are for Filesystem volume support where
 # blkid(8) and mkfs are required by k8s.io/mount-utils.
-RUN dnf install -qy nbd qemu-img util-linux-core e2fsprogs xfsprogs && dnf clean all
+RUN dnf update -y && dnf install --nodocs --noplugins -qy nbd qemu-img util-linux-core e2fsprogs xfsprogs && dnf clean all
 
 WORKDIR /kubesan
 
-COPY --from=makefile /kubesan/bin/kubesan bin/
+COPY --from=builder /kubesan/bin/kubesan bin/
+
+# TODO Should we use a nonroot user?
+# USER 65532:65532
 
 ENTRYPOINT [ "/kubesan/bin/kubesan" ]

commit 38be42cdc96ab8cbbb42eb0439a31f614e133964

maint: Split Containerfile into CI vs deployment container
Our recent attempts to improve Containerfile have led to hung
cross-arch builds, and more recently, complete failure to run the
testsuite if a cached build is not already present.  Among the
problems:

- golangci-lint depends on diff being present; but the bare-bones
  golang:1.22 package we base on does not include it.  Regressed in
  commit 8fddf3f7 ("containerfile best-practices updates")

- golangci-lint behaves differently when a configuration file is
  present; if this file is not shared between developer builds and
  container builds, then the container build will be attempting to run
  a different set of lints than the developer, which leads to needless
  confusion.  Regressed in commit 27a2ab4e ("build: Fix tests/run.sh
  after local `make` run")

However, it is also worth observing that when building multi-arch, it
makes more sense to only run golangci-lint once, on the build
platform.  There is no need to rerun it when cross-compiling.  Thus,
we have two competing needs:

- on CI, where go is not present natively, we want to do a single-arch
  build on native architecture, and we want this build to verify that
  controller-gen and lint steps are done (the CI is verifying that the
  developer checked in all generated code and fixed all coding issues
  that we enforce, so that git is self-contained)

- on release, we can assume that the code is already clean and passing
  CI, so all we want to do is build a multi-arch manifest without
  repeating those preliminary steps

This patch improves things by splitting all lint-like actions into a
separate tests/Containerfile, and arranges for CI to still call those
steps.  It requires adding a new 'make bin/kubesan' target, marked
PHONY because that's simpler in duplicating go's ability to track
dependencies.  The existing 'make', 'make all', and 'make build'
targets stay unchanged, but are no longer good things to invoke from a

Containerfile
@@ -2,6 +2,10 @@
 ARG TARGETOS
 ARG TARGETARCH
 ARG TARGETPLATFORM
+
+# This file is used to generate the container image to be loaded into
+# a KubeSAN deployment.  It assumes that the caller has already prepared
+# all necessary generated files; for linting, use tests/Containerfile.
 FROM quay.io/projectquay/golang:1.22 AS builder
 
 WORKDIR /kubesan
@@ -11,25 +15,15 @@ COPY go.mod go.sum Makefile ./
 COPY vendor vendor/
 RUN go mod verify
 
-# Although "make" will also build these tools, pre-building them as a
-# separate RUN now lets us cache things for faster container reuse
-RUN make controller-gen golangci-lint
-
 # Copy only source files, not generated binaries, since binaries built
 # in the host environment may not work in the container environment.
-# If .generate.timestamp exists in the host, we assume that the files
-# generated by controller-gen are already up-to-date and don't need to
-# be rebuilt; use of the ? allows the glob to pass if the file does not
-# exist, such as in a CI environment on a fresh checkout.
-COPY PROJECT .generate?timestamp ./
 COPY api/ api/
 COPY cmd/ cmd/
 COPY deploy/ deploy/
-COPY hack/ hack/
 COPY internal/ internal/
 
 # We set GOOS and GOARCH so go cross-compiles to the correct os and arch
-RUN GOOS=$TARGETOS GOARCH=$TARGETARCH make build
+RUN GOOS=$TARGETOS GOARCH=$TARGETARCH make bin/kubesan
 
 # CentOS Stream 9 doesn't provide package nbd
 # FROM quay.io/centos/centos:stream9

Makefile
@@ -56,7 +56,12 @@ help: ## Display this help.
 ##@ Build
 
 .PHONY: build
-build: .generate.timestamp vet lint ## Build the KubeSAN image locally.
+build: .generate.timestamp vet lint bin/kubesan ## Generate files and build the KubeSAN image locally.
+
+# Even though bin/kubesan is a real file, we don't want to spell out all of
+# the .go file dependencies; it's easier to just run go build unconditionally.
+.PHONY: bin/kubesan
+bin/kubesan: ## Build just the KubeSAN image, locally or in a container.
 	go build -mod=vendor --ldflags "-s -w" -a -o bin/kubesan cmd/main.go
 
 .PHONY: container

containerized-build.sh
@@ -1,6 +1,12 @@
+#!/bin/sh
 # SPDX-License-Identifier: Apache-2.0
+
+# This file is run by CI
 
 set -e -x
 
-#Build kubesan image
+# Build the linting image
+podman build -f tests/Containerfile .
+
+# Build and tag the kubesan image
 podman build -t kubesan:latest .

tests/Containerfile
@@ -1,0 +1,28 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# This file runs all lint-like tests that should be validated in CI prior to
+# building the top-level Containerfile.  It is generally built via the
+# containerized-build.sh script, and doesn't need to be tagged or uploaded.
+
+FROM quay.io/projectquay/golang:1.22
+
+# Needed for golangci-lint
+RUN dnf install -qy diffutils && dnf clean all
+
+WORKDIR /kubesan
+
+# Although "make" will also build these tools, pre-building them as a
+# separate RUN now lets us cache things for faster container reuse
+COPY go.mod go.sum Makefile .golangci.yml PROJECT ./
+RUN make controller-gen golangci-lint
+
+# Copy the files to be linted. This skips vendor/ on purpose.
+COPY api/ api/
+COPY cmd/ cmd/
+COPY deploy/ deploy/
+COPY hack/ hack/
+COPY internal/ internal/
+
+RUN make generate
+
+RUN make vet lint