kubesan/kubesan

commit 8fddf3f73e48fdbb2d9e4819ae9c832aaf3414ef

containerfile best-practices updates
We are updating a few things in the containerfile:
- Adding in TARGETOS TARGETARCH and TARGETPLATFORM args
  - These allow us to cross-compile and do multiarch builds
- Switched the builder image name from 'makefile' to the more standard 'builder'
- Switched to more generic base images which allows users to pull from
  their preferred repository
- Added the GOOS and GOARCH environment variables to the go build
- Added the platform flag to the fedora base image for multiarch builds
- Added dnf update to make sure all security patches are in the base image
- Added --nodocs and --noplugins for a slimmed down dnf install

Signed-off-by: Jeff Roche <jeroche@redhat.com>
[eblake: also fix a stale comment, hoist vendoring change to earlier patch,
drop attempt to use non-root user for a later patch]
Signed-off-by: Eric Blake <eblake@redhat.com>

Containerfile
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
-
-FROM quay.io/projectquay/golang:1.22 AS makefile
-
-RUN dnf install -qy diffutils && dnf clean all
+ARG TARGETOS
+ARG TARGETARCH
+ARG TARGETPLATFORM
+FROM golang:1.22 AS builder
 
 WORKDIR /kubesan
 
@@ -28,18 +28,24 @@ COPY deploy/ deploy/
 COPY hack/ hack/
 COPY internal/ internal/
 
-RUN make build
+# We set GOOS and GOARCH so go cross-compiles to the correct os and arch
+RUN GOOS=$TARGETOS GOARCH=$TARGETARCH make build
 
 # CentOS Stream 9 doesn't provide package nbd
 # FROM quay.io/centos/centos:stream9
-FROM quay.io/fedora/fedora:40
+# We use --platform=$TARGETPLATFORM to pull the correct arch for
+# the base image. This is needed for multi-arch builds
+FROM --platform=$TARGETPLATFORM fedora:40
 
 # util-linux-core, e2fsprogs, and xfsprogs are for Filesystem volume support where
 # blkid(8) and mkfs are required by k8s.io/mount-utils.
-RUN dnf install -qy nbd qemu-img util-linux-core e2fsprogs xfsprogs && dnf clean all
+RUN dnf update -y && dnf install --nodocs --noplugins -qy nbd qemu-img util-linux-core e2fsprogs xfsprogs && dnf clean all
 
 WORKDIR /kubesan
 
-COPY --from=makefile /kubesan/bin/kubesan bin/
+COPY --from=builder /kubesan/bin/kubesan bin/
+
+# TODO Should we use a nonroot user?
+# USER 65532:65532
 
 ENTRYPOINT [ "/kubesan/bin/kubesan" ]

commit 543bcf9fa8db03f2a784d02f3b59f988511d37c4

maint: Improve multi-arch release
When building a multi-arch release, podman defaults to remembering
which architecture last provided a given container:tag.  If the
Containerfile does not override things with an explicit --platform,
then this can end up using a non-native container (and hence slower
qemu emulation) on the second build, when reusing the container from
the arch of the first build.  But we always want our builder container
to be a native binary (with the previous patch, we are using it solely
to produce cross-compiler output for the desired platform to be copied
into the distribution container).

Furthermore, if the user manually passes in a list of platforms, it is
nice to detect early on if the base containers support all of those
platforms.  Pulling the container in before running all the builder
steps can lead to faster failures if the user requests an unsupported
combination.

And since the recipe for building a multi-arch release is complex
enough to be difficult to remember, yet something that only needs to
be done at release time, it is worth adding a 'make
container-multiarch' and using it from within ./release.sh.

CC: Jeff Roche <jeroche@redhat.com>
Fixes: 8fddf3f7 ("containerfile best-practices updates")
Signed-off-by: Eric Blake <eblake@redhat.com>

Containerfile
@@ -1,4 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
+ARG BUILDPLATFORM
 ARG TARGETOS
 ARG TARGETARCH
 ARG TARGETPLATFORM
@@ -6,7 +7,17 @@ ARG TARGETPLATFORM
 # This file is used to generate the container image to be loaded into
 # a KubeSAN deployment.  It assumes that the caller has already prepared
 # all necessary generated files; for linting, use tests/Containerfile.
-FROM quay.io/projectquay/golang:1.22 AS builder
+
+## --target target
+# This is the default target.  It is pulled first to ensure we have the
+# right architectures, while still allowing the builder to run natively.
+# CentOS Stream 9 doesn't provide package nbd
+# FROM quay.io/centos/centos:stream9
+FROM --platform=$TARGETPLATFORM quay.io/fedora/fedora:40 as target
+
+## --target builder
+# This target builds the kubesan binary, possibly with cross-compilation.
+FROM --platform=$BUILDPLATFORM quay.io/projectquay/golang:1.22 AS builder
 
 WORKDIR /kubesan
 
@@ -25,11 +36,8 @@ COPY internal/ internal/
 # We set GOOS and GOARCH so go cross-compiles to the correct os and arch
 RUN GOOS=$TARGETOS GOARCH=$TARGETARCH make bin/kubesan
 
-# CentOS Stream 9 doesn't provide package nbd
-# FROM quay.io/centos/centos:stream9
-# We use --platform=$TARGETPLATFORM to pull the correct arch for
-# the base image. This is needed for multi-arch builds
-FROM --platform=$TARGETPLATFORM quay.io/fedora/fedora:40
+## Back to the default target
+FROM target
 
 # util-linux-core, e2fsprogs, and xfsprogs are for Filesystem volume support where
 # blkid(8) and mkfs are required by k8s.io/mount-utils.

Makefile
@@ -26,7 +26,9 @@ else
 GOBIN=$(shell go env GOBIN)
 endif
 
+# Set CONTAINER_PLATFORMS=--all-platforms to see if this static list needs to grow
 CONTAINER_TOOL ?= podman
+CONTAINER_PLATFORMS ?= --platform=linux/amd64,linux/ppc64le,linux/s390x,linux/arm64
 
 # Setting SHELL to bash allows bash commands to be executed by recipes.
 # Options are set to exit when a recipe line exits non-zero or a piped command fails.
@@ -65,8 +67,13 @@ bin/kubesan: ## Build just the KubeSAN image, locally or in a container.
 	go build -mod=vendor --ldflags "-s -w" -a -o bin/kubesan cmd/main.go
 
 .PHONY: container
-container:  ## Build the KubeSAN container.
+container: ## Build a single-arch KubeSAN container, for testing.
 	$(CONTAINER_TOOL) build -t $(IMG) .
+
+.PHONY: container-multiarch
+container-multiarch: ## Build a multi-arch KubeSAN container, for release.
+	$(CONTAINER_TOOL) manifest create --amend $(IMG)
+	$(CONTAINER_TOOL) build $(CONTAINER_PLATFORMS) --manifest $(IMG) .
 
 ##@ Development
 

release.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # SPDX-License-Identifier: Apache-2.0
 
 set -e
@@ -6,6 +7,11 @@ GREEN='\033[0;32m'
 RESET='\033[0m'
 VERSION=$1
 PREV_VERSION=$2
+
+if test $# != 2; then
+    echo "Usage: $0 VERS PREV_VERS"
+    exit 1
+fi
 
 # Update files with version number
 printf "${GREEN}Updating version number in files${RESET}\n"
@@ -18,13 +24,14 @@ sed -i "s|kubesan/kubesan/deploy/kubernetes?ref=${PREV_VERSION}|kubesan/kubesan/
 git add internal/common/config/config.go deploy/kubernetes/kustomization.yaml docs/1-getting-started.md
 git commit -s -m "Release ${VERSION}" -e
 
-# Run tests 
+# Run tests
 printf "${GREEN}Running tests${RESET}\n"
 tests/run.sh all
 
 # Publish container image
 printf "${GREEN}Publish container image${RESET}\n"
-podman push localhost/kubesan:latest quay.io/kubesan/kubesan:${VERSION}
+make container-multiarch CONTAINER_TOOL=podman IMG=quay.io/kubesan/kubesan:${VERSION}
+podman manifest push quay.io/kubesan/kubesan:${VERSION}
 
 # Publish git tag
 printf "${GREEN}Publishing git tag${RESET}\n"
@@ -41,4 +48,3 @@ git commit -s -m "Reopen development after ${VERSION}"
 # Push commits
 printf "${GREEN}Pushing commits to main${RESET}\n"
 git push origin HEAD
-

tests/Containerfile
@@ -1,10 +1,11 @@
 # SPDX-License-Identifier: Apache-2.0
+ARG BUILDPLATFORM
 
 # This file runs all lint-like tests that should be validated in CI prior to
 # building the top-level Containerfile.  It is generally built via the
 # containerized-build.sh script, and doesn't need to be tagged or uploaded.
 
-FROM quay.io/projectquay/golang:1.22
+FROM --platform=$BUILDPLATFORM quay.io/projectquay/golang:1.22
 
 # Needed for golangci-lint
 RUN dnf install -qy diffutils && dnf clean all