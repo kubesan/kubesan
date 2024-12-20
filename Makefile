kubesan/kubesan

commit 3e4f3590d92997c41a9ac3222705de8bf8adc797

update golangci-lint to the latest version to resolve issues running podman builds and tests
Signed-off-by: Jeff Roche <jeroche@redhat.com>

Makefile
@@ -11,7 +11,7 @@ LOCALBIN ?= $(shell pwd)/bin
 ## Tool Versions
 CONTROLLER_TOOLS_VERSION ?= v0.14.0
 ENVTEST_VERSION ?= release-0.17
-GOLANGCI_LINT_VERSION ?= v1.57.2
+GOLANGCI_LINT_VERSION ?= v1.62.2
 
 ## Tool Binaries
 KUBECTL ?= kubectl

commit 13a78b2a283d009d9bfd4196412212c812986a95

maint: Improve the quality of 'make lint'
Ever since we bumped to a newer version of golangci-lint in commit
3e4f3590 ("update golangci-lint to the latest version..."), 'make
lint' has been complaining:

  WARN The linter 'exportloopref' is deprecated (since v1.60.2) due to: Since Go1.22 (loopvar) this linter is no longer relevant. Replaced by copyloopvar.

Time to update the lint configuration file to use the modern spelling,
as well as to also enable several other useful linters, most of which
passed out of the box, although a couple of code tweaks were needed
(one to obey a suggestion from perfsprint, another to silence a false
positive from gosec on our use of external commands).  It's also worth
mandating that the linter flag any problems that would require us to
update 'make vendor'.

Note that there are still problems with running the linter inside a
container which will be addressed in other patches; this patch focuses
only on 'make lint' in a developer's checkout.  I also included a
change to Makefile that made it easier for me to test other versions
of golangci-lint.

Fixes: 3e4f3590
Signed-off-by: Eric Blake <eblake@redhat.com>

.golangci.yml
@@ -1,6 +1,7 @@
 run:
   timeout: 5m
   allow-parallel-runners: true
+  modules-download-mode: readonly
 
 issues:
   # don't skip warning about doc comments
@@ -19,20 +20,30 @@ issues:
 linters:
   disable-all: true
   enable:
+    - asciicheck
+    - bidichk
+    - copyloopvar
     - dupl
+    - dupword
     - errcheck
-    - exportloopref
+    - fatcontext
     - goconst
     - gocyclo
     - gofmt
     - goimports
+    - gosec
     - gosimple
     - govet
     - ineffassign
     - lll
     - misspell
     - nakedret
+    - nilerr
+    - nilnil
+    - perfsprint
     - prealloc
+    - predeclared
+    - reassign
     - staticcheck
     - typecheck
     - unconvert

Makefile
@@ -17,7 +17,7 @@ GOLANGCI_LINT_VERSION ?= v1.62.2
 KUBECTL ?= kubectl
 CONTROLLER_GEN ?= $(LOCALBIN)/controller-gen-$(CONTROLLER_TOOLS_VERSION)
 ENVTEST ?= $(LOCALBIN)/setup-envtest-$(ENVTEST_VERSION)
-GOLANGCI_LINT = $(LOCALBIN)/golangci-lint-$(GOLANGCI_LINT_VERSION)
+GOLANGCI_LINT ?= $(LOCALBIN)/golangci-lint-$(GOLANGCI_LINT_VERSION)
 
 # Get the currently used golang install path (in GOPATH/bin, unless GOBIN is set)
 ifeq (,$(shell go env GOBIN))

internal/common/commands/commands.go
@@ -22,7 +22,7 @@ type Output struct {
 
 // If the command exits with a non-zero status, an error is returned alongside the output.
 func RunInContainerContext(ctx context.Context, command ...string) (Output, error) {
-	cmd := exec.CommandContext(ctx, command[0], command[1:]...)
+	cmd := exec.CommandContext(ctx, command[0], command[1:]...) //nolint:gosec
 	cmd.Env = append(cmd.Environ(), "LC_ALL=C")
 	cmd.Stdin = nil
 

internal/common/nbd/server.go
@@ -205,7 +205,7 @@ func nodeName(export string) string {
 
 // Returns the QMP block export id given an NBD export name.
 func blockExportId(export string) string {
-	return fmt.Sprintf("export-%s", export)
+	return "export-" + export
 }
 
 // Returns success only once the server is running and has the TCP port open.