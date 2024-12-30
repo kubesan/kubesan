65934552d42dbf5400656406a8c2a11d2b412753
Eric Blake <eblake@redhat.com>
2024-12-30 15:44:54 -0600

    nbd: Add NBD cleanup support to Volume
    Of course, until something creates the NBD server and clients in the
    first place, there is nothing to clean up.  But doing things
    incrementally makes it easier to review each piece as it is going in.
    
    This covers both the NBD server case (stop serving if there are no
    more interested clients) and the client case (stop the client if the
    server needs to go away).  Since more code paths can fail with
    WatchPending, this also moves that handling to a more central
    location.
    
    It also does a bit more cleanup of the reconcileThinDetaching()
    function - in particular, we should remove any device-mapper wrapper
    whether or not the node doing an unstage is actively using the LV
    (note that this means that on a large cluster, any reconcile action
    will trigger a dmsetup remove call on all nodes where the volume is
    not staged, including nodes where the dm device was never created;
    maybe that can be optimized to first check for existence).  Then, we
    must ensure that we hand over control of the LV whether we currently
    own it (Status.ActiveOnNode) or have previously made a claim to own it
    (Spec.ActiveOnNode); the use of WatchPending makes sure that we don't
    clear Volume.Status.AttachedToNodes prematurely.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>

--- internal/manager/node/volume_node_controller.go
+++ internal/manager/node/volume_node_controller.go
@@ -9,6 +9,7 @@ import (
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -19,6 +20,7 @@ import (
 	"gitlab.com/kubesan/kubesan/internal/common/commands"
 	"gitlab.com/kubesan/kubesan/internal/common/config"
 	"gitlab.com/kubesan/kubesan/internal/common/dm"
+	"gitlab.com/kubesan/kubesan/internal/common/nbd"
 	kubesanslices "gitlab.com/kubesan/kubesan/internal/common/slices"
 	"gitlab.com/kubesan/kubesan/internal/manager/common/thinpoollv"
 	"gitlab.com/kubesan/kubesan/internal/manager/common/util"
@@ -44,8 +46,8 @@ func SetUpVolumeNodeReconciler(mgr ctrl.Manager) error {
 // +kubebuilder:rbac:groups=kubesan.gitlab.io,resources=volumes,verbs=get;list;watch;create;update;patch;delete,namespace=kubesan-system
 // +kubebuilder:rbac:groups=kubesan.gitlab.io,resources=volumes/status,verbs=get;update;patch,namespace=kubesan-system
 
-// Ensure that the volume is attached to this node
-// May fail with WatchPending if another reconcile will trigger progress
+// Ensure that the volume is attached to this node.
+// May fail with WatchPending if another reconcile will trigger progress.
 func (r *VolumeNodeReconciler) reconcileThinAttaching(ctx context.Context, volume *v1alpha1.Volume, thinPoolLv *v1alpha1.ThinPoolLv) error {
 	oldThinPoolLv := thinPoolLv.DeepCopy()
 	log := log.FromContext(ctx).WithValues("nodeName", config.LocalNodeName)
@@ -79,31 +81,90 @@ func (r *VolumeNodeReconciler) reconcileThinAttaching(ctx context.Context, volum
 	return dm.Resume(ctx, volume.Name, volume.Spec.SizeBytes, devName(volume))
 }
 
-// Ensure that the volume is detached from this node
+// Ensure that the volume is detached from this node.
 func (r *VolumeNodeReconciler) reconcileThinDetaching(ctx context.Context, volume *v1alpha1.Volume, thinPoolLv *v1alpha1.ThinPoolLv) error {
 	oldThinPoolLv := thinPoolLv.DeepCopy()
-
-	if thinPoolLv.Status.ActiveOnNode != config.LocalNodeName {
-		if thinPoolLv.Spec.ActiveOnNode == config.LocalNodeName {
-			// clear thinPoolLv.Spec.ActiveOnNode once activation is no longer required
-			return thinpoollv.UpdateThinPoolLv(ctx, r.Client, oldThinPoolLv, thinPoolLv)
-		}
-		return nil // it's not attached to this node
-	}
 
 	if err := dm.Remove(ctx, volume.Name); err != nil {
 		return err
 	}
 
+	if thinPoolLv.Status.ActiveOnNode != config.LocalNodeName && thinPoolLv.Spec.ActiveOnNode != config.LocalNodeName {
+		return nil // it's not attached to this node
+	}
+
 	thinLvName := thinpoollv.VolumeToThinLvName(volume.Name)
 	thinLvSpec := thinPoolLv.Spec.FindThinLv(thinLvName)
-	if thinLvSpec != nil && thinLvSpec.State.Name == v1alpha1.ThinLvSpecStateNameActive {
-		thinLvSpec.State = v1alpha1.ThinLvSpecState{
-			Name: v1alpha1.ThinLvSpecStateNameInactive,
+	// TODO: Do not mark this LV inactive if there is still some other
+	// snapshot being created in the same thin pool
+	if len(volume.Spec.AttachToNodes) == 0 {
+		if thinLvSpec != nil && thinLvSpec.State.Name == v1alpha1.ThinLvSpecStateNameActive {
+			thinLvSpec.State.Name = v1alpha1.ThinLvSpecStateNameInactive
+		}
+	} else {
+		thinPoolLv.Spec.ActiveOnNode = volume.Spec.AttachToNodes[0]
+	}
+
+	if err := thinpoollv.UpdateThinPoolLv(ctx, r.Client, oldThinPoolLv, thinPoolLv); err != nil {
+		return err
+	}
+	if thinPoolLv.Status.ActiveOnNode == config.LocalNodeName {
+		return &util.WatchPending{}
+	}
+	return nil
+}
+
+// Handle any NBD client or server that needs to be cleaned up before
+// detaching or migrating a node activation.
+func (r *VolumeNodeReconciler) reconcileThinNBDCleanup(ctx context.Context, volume *v1alpha1.Volume) error {
+	nbdExport := &v1alpha1.NBDExport{}
+	log := log.FromContext(ctx).WithValues("nodeName", config.LocalNodeName)
+
+	if volume.Status.NBDExport == "" {
+		return nil
+	}
+	err := r.Get(ctx, types.NamespacedName{Name: volume.Status.NBDExport, Namespace: config.Namespace}, nbdExport)
+	if errors.IsNotFound(err) {
+		volume.Status.NBDExport = ""
+		err = r.statusUpdate(ctx, volume)
+	}
+	if err != nil {
+		return err
+	}
+
+	connected, disconnect := nbd.CheckClient(ctx, r.Client, nbdExport)
+	if (connected && !slices.Contains(volume.Spec.AttachToNodes, config.LocalNodeName)) || disconnect {
+		log.Info("Cleaning up NBD client", "export", volume.Status.NBDExport)
+		if err := dm.Suspend(ctx, volume.Name, volume.Spec.Type.Block != nil); err != nil {
+			return err
+		}
+		if err := nbd.DisconnectClient(ctx, r.Client, nbdExport); err != nil {
+			return err
 		}
 	}
 
-	return thinpoollv.UpdateThinPoolLv(ctx, r.Client, oldThinPoolLv, thinPoolLv)
+	if nbd.ShouldStopExport(nbdExport, volume.Spec.AttachToNodes) {
+		log.Info("Cleaning up NBD server", "export", volume.Status.NBDExport)
+		if nbdExport.Spec.Path != "" {
+			nbdExport.Spec.Path = ""
+			if err := r.Update(ctx, nbdExport); err != nil {
+				return err
+			}
+		}
+		if len(nbdExport.Spec.Clients) == 0 {
+			propagation := client.PropagationPolicy(metav1.DeletePropagationBackground)
+			if err := r.Delete(ctx, nbdExport, propagation); err != nil && !errors.IsNotFound(err) {
+				return err
+			}
+			volume.Status.NBDExport = ""
+			if err := r.statusUpdate(ctx, volume); err != nil {
+				return err
+			}
+		} else {
+			return &util.WatchPending{}
+		}
+	}
+	return nil
 }
 
 func isThinLvActiveOnLocalNode(thinPoolLv *v1alpha1.ThinPoolLv, name string) bool {
@@ -111,7 +172,7 @@ func isThinLvActiveOnLocalNode(thinPoolLv *v1alpha1.ThinPoolLv, name string) boo
 	return thinLvStatus != nil && thinPoolLv.Status.ActiveOnNode == config.LocalNodeName && thinLvStatus.State.Name == v1alpha1.ThinLvStatusStateNameActive
 }
 
-// Update Volume.Status.AttachedToNodes[] from the ThinPoolLv
+// Update Volume.Status.AttachedToNodes[] from the ThinPoolLv.
 func (r *VolumeNodeReconciler) updateStatusAttachedToNodes(ctx context.Context, volume *v1alpha1.Volume, thinPoolLv *v1alpha1.ThinPoolLv) error {
 	thinLvName := thinpoollv.VolumeToThinLvName(volume.Name)
 
@@ -142,11 +203,14 @@ func devName(volume *v1alpha1.Volume) string {
 
 func (r *VolumeNodeReconciler) reconcileThin(ctx context.Context, volume *v1alpha1.Volume) error {
 	thinPoolLv := &v1alpha1.ThinPoolLv{}
-	log := log.FromContext(ctx).WithValues("nodeName", config.LocalNodeName)
 
 	err := r.Get(ctx, types.NamespacedName{Name: volume.Name, Namespace: config.Namespace}, thinPoolLv)
 	if err != nil {
 		return client.IgnoreNotFound(err)
+	}
+
+	if err = r.reconcileThinNBDCleanup(ctx, volume); err != nil {
+		return err
 	}
 
 	if slices.Contains(volume.Spec.AttachToNodes, config.LocalNodeName) {
@@ -156,10 +220,6 @@ func (r *VolumeNodeReconciler) reconcileThin(ctx context.Context, volume *v1alph
 	}
 
 	if err != nil {
-		if _, ok := err.(*util.WatchPending); ok {
-			log.Info("reconcileThin waiting for Watch")
-			return nil // wait until Watch triggers
-		}
 		return err
 	}
 
@@ -247,6 +307,11 @@ func (r *VolumeNodeReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 		err = r.reconcileLinear(ctx, volume)
 	default:
 		err = errors.NewBadRequest("invalid volume mode")
+	}
+
+	if _, ok := err.(*util.WatchPending); ok {
+		log.Info("reconcile waiting for Watch")
+		return ctrl.Result{}, nil // wait until Watch triggers
 	}
 
 	return ctrl.Result{}, err
