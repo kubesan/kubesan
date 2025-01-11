2f4b0dae7e9f40df4ae0c0d35ee2919ede102c56
Eric Blake <eblake@redhat.com>
2025-01-11 17:15:16 -0600

    nbd: Test restarting a pod serving NBD
    The NBD service cannot survive a pod respawn, but because of the
    cleanup hooks that previous patches just added, all clients have
    temporarily paused device-mapper I/O across the restart and are able
    to reconnect as soon as the new pod and NBD server is up and running.
    Enhance the existing nbd-pod-restart test case to show that we now
    support uninterrupted RWX clients regardless of which node manager pod
    is restarted.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>

--- tests/t/nbd-pod-restart.sh
+++ tests/t/nbd-pod-restart.sh
@@ -1,10 +1,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
-# This test checks that a node using secondary access (the NBD client)
-# can survive a reset of the node manager pod, since an NBD client is
-# independent of the manager once set up.  (Note, however, that the
-# converse of the primary node manager being reset while serving NBD is
-# a non-goal, as resetting kills the NBD connection).
+# This test checks that RWX connections survive pod restarts.  This
+# works for both the secondary node (the NBD client connection is
+# independent of the pod once started; and the new pod sees the
+# existing client), and the primary node (the NBD server has a
+# graceful shutdown that waits for all clients to first disconnect
+# before the new pod starts a new server that clients can connect to).
 ksan-supported-modes Linear Thin
 
 ksan-stage 'Provisioning volume...'
@@ -45,18 +46,34 @@ for i in "${!NODES[@]}"; do
     ksan-wait-for-pod-to-start-running 60 "test-pod-$i"
 done
 
-ksan-stage 'Forcing restart of node manager...'
-# Kill the node manager...
+# find_manager <idx>: return the name of the node manager pod on node idx
 find_manager() {
     kubectl --namespace kubesan-system get pods --output name \
-            --field-selector spec.nodeName=${NODES[1]} \
+            --field-selector spec.nodeName=${NODES[$1]} \
             --selector app.kubernetes.io/component=node-controller-manager
 }
-victim=$(find_manager)
+
+ksan-stage 'Forcing restart of secondary node manager...'
+# Kill the node manager...
+victim=$(find_manager 1)
 kubectl delete --namespace kubesan-system ${victim} --timeout=30s
 
 # ...then wait for the DaemonSet to respawn its replacement
-ksan-poll 1 30 "[[ \$(find_manager | wc -l) == 1 && \$(find_manager) != ${victim} ]]"
+ksan-poll 1 30 "[[ \$(find_manager 1 | wc -l) == 1 && \$(find_manager 1) != ${victim} ]]"
+
+ksan-stage 'Ensuring pods survived...'
+sleep 2
+for i in "${!NODES[@]}"; do
+    ksan-pod-is-running "test-pod-$i"
+done
+
+ksan-stage 'Forcing restart of primary node manager...'
+# Kill the node manager...
+victim=$(find_manager 0)
+kubectl delete --namespace kubesan-system ${victim} --timeout=30s
+
+# ...then wait for the DaemonSet to respawn its replacement
+ksan-poll 1 30 "[[ \$(find_manager 0 | wc -l) == 1 && \$(find_manager 0) != ${victim} ]]"
 
 ksan-stage 'Ensuring pods survived...'
 sleep 2
