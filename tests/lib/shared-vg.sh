443f28e0991ce2d7b7a10d069943dd30fd120585
Stefan Hajnoczi <stefanha@redhat.com>
2025-01-16 16:58:30 -0500

    tests: Set up LVM devices files like in production
    The test environment should be as similar to production environments as
    possible so it triggers the same bugs. Currently system.devices contains
    the device and this makes thin-pool automatic extension work in the
    absence of a dmeventd.devices file.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- tests/lib/shared-vg.sh
+++ tests/lib/shared-vg.sh
@@ -3,14 +3,25 @@
 # usage __create_ksan_sharedvg <vgname> <device>
 __create_ksan_shared_vg() {
     __${deploy_tool}_ssh "${NODES[0]}" "
-        sudo vgcreate --shared "$1" "$2"
+        sudo vgcreate --devicesfile "$1" --shared "$1" "$2"
     "
 
+    # The devices file setup is important:
+    # - system.devices must exist so that LVM refrains from scanning all PVs on
+    #   the system and only limits itself to a devices file. This ensures that
+    #   KubeSAN PVs are not seen by non-KubeSAN LVM users. Although it is not
+    #   important in a test environment, it's the setup we recommend in
+    #   production and therefore we test it.
+    # - dmeventd.devices must contain the KubeSAN PVs so that automatic
+    #   extension of thin-pools works.
+    # - The <vgname> devices file must contain the KubeSAN PVs. This is the
+    #   devices file that KubeSAN's LVM commands use.
     for node in "${NODES[@]}"; do
         __${deploy_tool}_ssh "${node}" "
-        sudo lvmdevices --devicesfile "$1" --adddev "$2"
-        sudo vgchange --lockstart "$1"
+        sudo touch /etc/lvm/devices/system.devices
         sudo vgimportdevices "$1" --devicesfile "$1"
+        sudo vgimportdevices "$1" --devicesfile dmeventd.devices
+        sudo vgchange --devicesfile "$1" --lockstart "$1"
         "
     done
 }
