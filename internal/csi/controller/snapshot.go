kubesan/kubesan

commit 9051f5cfdf6302a6ef712cbdd2a6dd7fd3506b69

snapshot: Add more CSI sanity validations
Make sure that we are only attempting snapshots of thin blocks (we can
probably also do thin filesystems, but that would be better if we
freeze and thaw the filesystem around the snapshot creation).  Also,
the CSI spec requires us to diagnose attempts to create a snapshot
with an invalid source, or to recreate a snapshot with a different
source.

Signed-off-by: Eric Blake <eblake@redhat.com>

internal/csi/controller/snapshot.go
@@ -28,12 +28,36 @@ import (
 func (s *ControllerServer) CreateSnapshot(ctx context.Context, req *csi.CreateSnapshotRequest) (*csi.CreateSnapshotResponse, error) {
 	// validate request
 
-	if _, err := validate.ValidateVolumeID(req.SourceVolumeId); err != nil {
+	namespacedName, err := validate.ValidateVolumeID(req.SourceVolumeId)
+	if err != nil {
+		return nil, err
+	}
+	volume := &v1alpha1.Volume{}
+	err = s.client.Get(ctx, namespacedName, volume)
+	if errors.IsNotFound(err) {
+		return nil, status.Error(codes.NotFound, "volume not found")
+	}
+	if err != nil {
 		return nil, err
 	}
 
-	if req.Name == "" {
+	switch {
+	case volume.Spec.Mode != v1alpha1.VolumeModeThin:
+		return nil, status.Error(codes.Unimplemented, "snapshots are only supported on thin volumes")
+
+	case volume.Spec.Type.Filesystem != nil:
+		return nil, status.Error(codes.Unimplemented, "filesystem snapshots not implemented yet")
+
+	case req.Name == "":
 		return nil, status.Error(codes.InvalidArgument, "must specify snapshot name")
+
+	// At present, we don't use secrets, so this should be empty.
+	case len(req.Secrets) > 0:
+		return nil, status.Error(codes.InvalidArgument, "unexpected secrets")
+
+	// At present, we don't support any parameters, so this should be empty.
+	case len(req.Parameters) > 0:
+		return nil, status.Error(codes.InvalidArgument, "unexpected parameters")
 	}
 
 	// create snapshot
@@ -49,11 +73,20 @@ func (s *ControllerServer) CreateSnapshot(ctx context.Context, req *csi.CreateSn
 	}
 	controllerutil.AddFinalizer(snapshot, config.Finalizer)
 
-	if err := s.client.Create(ctx, snapshot); err != nil && !errors.IsAlreadyExists(err) {
+	err = s.client.Create(ctx, snapshot)
+	if errors.IsAlreadyExists(err) {
+		// Check that the new request is idempotent to the existing snapshot
+		err = s.client.Get(ctx, types.NamespacedName{Name: snapshot.Name, Namespace: config.Namespace}, snapshot)
+		if err == nil && req.SourceVolumeId != snapshot.Spec.SourceVolume {
+			err = status.Error(codes.AlreadyExists, "snapshot already exists with different source")
+
+		}
+	}
+	if err != nil {
 		return nil, err
 	}
 
-	err := s.client.WatchSnapshotUntil(ctx, snapshot, func() bool {
+	err = s.client.WatchSnapshotUntil(ctx, snapshot, func() bool {
 		return meta.IsStatusConditionTrue(snapshot.Status.Conditions, v1alpha1.SnapshotConditionAvailable)
 	})
 	if err != nil {

internal/manager/cluster/snapshot_controller.go
@@ -118,7 +118,6 @@ func (r *SnapshotReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 	// snapshot creation/deletion, so an empty string can be passed here.
 	vgName := ""
 
-	// TODO Return an error if the volume is Linear rather than Thin.
 	blobMgr := blobs.NewThinBlobManager(r.Client, r.Scheme, vgName, snapshot.Spec.SourceVolume)
 
 	var err error

commit 9650e5418ef8254c4dada6c480781e1007681b20

snapshot: Fix sanity checking of parameters
Our deployment asked CSI to hand us several "csi.storage.k8s.io/*"
extra parameters on top of what is present in the VolumeSnapshotClass,
which then caused tests/snapshot.sh to fail when len(req.Parameters)
was non-zero.  But since we aren't actually depending on those
parameters, let's just change the deployment to quit asking for them.

Do the same for volumes and StorageClass parameter synthesis.

(It doesn't help that we are still battling a case of gitlab merging
commits before Jenkins has had a chance to finish regression testing -
because otherwise this would have failed to merge earlier.)

Fixes: commit 9051f5cf ("snapshot: Add more CSI sanity validations")
Signed-off-by: Eric Blake <eblake@redhat.com>

deploy/kubernetes/csi/controller-plugin.yaml
@@ -71,7 +71,8 @@ spec:
         - name: csi-provisioner
           image: registry.k8s.io/sig-storage/csi-provisioner:v3.5.0
           args:
-            - --extra-create-metadata  # to get PVC/PV info in CreateVolume()
+            # See https://gitlab.com/kubesan/kubesan/-/issues/105:
+            # - --extra-create-metadata  # to get PVC/PV info in CreateVolume()
             - --default-fstype=ext4 # default FSType so that SecurityContext fsGroup works
           volumeMounts:
             - name: socket-dir
@@ -88,7 +89,8 @@ spec:
         - name: csi-snapshotter
           image: registry.k8s.io/sig-storage/csi-snapshotter:v6.3.3
           args:
-            - --extra-create-metadata  # to get VS/VSC info in CreateSnapshot()
+            # See https://gitlab.com/kubesan/kubesan/-/issues/105:
+            # - --extra-create-metadata  # to get VS/VSC info in CreateSnapshot()
           volumeMounts:
             - name: socket-dir
               mountPath: /run/csi

internal/csi/controller/snapshot.go
@@ -55,7 +55,11 @@ func (s *ControllerServer) CreateSnapshot(ctx context.Context, req *csi.CreateSn
 	case len(req.Secrets) > 0:
 		return nil, status.Error(codes.InvalidArgument, "unexpected secrets")
 
-	// At present, we don't support any parameters, so this should be empty.
+	// At present, we don't support any parameters, so this should
+	// be empty.  Note that passing --extra-create-metadata in
+	// controller-plugin.yaml would change this situation, but our
+	// current design does not need to get the VolumeSnapshot object.
+	// See https://gitlab.com/kubesan/kubesan/-/issues/105.
 	case len(req.Parameters) > 0:
 		return nil, status.Error(codes.InvalidArgument, "unexpected parameters")
 	}

internal/csi/controller/volume.go
@@ -38,10 +38,22 @@ const (
 )
 
 func (s *ControllerServer) CreateVolume(ctx context.Context, req *csi.CreateVolumeRequest) (*csi.CreateVolumeResponse, error) {
+	// Note that passing --extra-create-metadata in
+	// controller-plugin.yaml would give us access to the
+	// PersistentVolumeClaim object, but our current design does
+	// not need it.
+	// See https://gitlab.com/kubesan/kubesan/-/issues/105.
 	// pvName, err := getParameter("csi.storage.k8s.io/pv/name")
 	// if err != nil {
 	// 	return nil, err
 	// }
+
+	// The easiest way to reject unknown parameters is to see if a
+	// copy of only known parameters is the same length.
+	knownParameters := copyKnownParameters(req.Parameters)
+	if len(req.Parameters) > len(knownParameters) {
+		return nil, status.Error(codes.InvalidArgument, "unexpected parameters")
+	}
 
 	lvmVolumeGroup := req.Parameters["lvmVolumeGroup"]
 	if lvmVolumeGroup == "" {