kubesan/kubesan

commit 138b0bd7128f1b24206d998e23006bce268aac5c

csi: Add new wipePolicy:UnsafeFast parameter to StorageClass
While we must default to safe, there are clusters where defaulting to
fast is permissible.  Add a knob to allow the user to opt-in to not
wiping the contents of a Linear volume beyond the first 4M extent, for
users that are okay with the security risk.  Furthermore, it is always
safe to skip wiping on the tail end of a block that will only be used
for filesystems (a pod cannot see the uninitialized space behind a
filesystem).

We may add other wipe policies in the future, such as a wipe on delete
(if the VG starts clean, and we always wipe on delete, then it is safe
to skip wiping on creation -that slows down deletion of even thin
pools, unlike this patch, but slow deletion is less of an issue than
slow creation).

Partially fixes: https://gitlab.com/kubesan/kubesan/-/issues/101
Signed-off-by: Eric Blake <eblake@redhat.com>

internal/manager/common/blobs/blob_manager_linear.go
@@ -35,17 +35,27 @@ func NewLinearBlobManager(workers *workers.Workers, vgName string) BlobManager {
 }
 
 type blkdiscardWork struct {
-	vgName string
-	lvName string
-	offset int64
+	vgName   string
+	lvName   string
+	offset   int64
+	skipWipe bool
 }
 
 func (w *blkdiscardWork) Run(ctx context.Context) error {
 	log := log.FromContext(ctx)
+	if w.skipWipe && w.offset > 0 {
+		return nil
+	}
 	return commands.WithLvmLvActivated(w.vgName, w.lvName, func() error {
 		path := fmt.Sprintf("/dev/%s/%s", w.vgName, w.lvName)
-		log.Info("blkdiscard worker zeroing LV", "path", path)
-		_, err := commands.RunOnHostContext(ctx, "blkdiscard", "--zeroout", "--offset", strconv.FormatInt(w.offset, 10), path)
+		args := []string{"blkdiscard", "--zeroout", "--offset",
+			strconv.FormatInt(w.offset, 10)}
+		if w.skipWipe {
+			args = append(args, "--length", "4Mi")
+		}
+		args = append(args, path)
+		log.Info("blkdiscard worker zeroing LV", "path", path, "command", args)
+		_, err := commands.RunOnHostContext(ctx, args...)
 		// To test long-running operations: _, err := commands.RunOnHostContext(ctx, "sleep", "30")
 		log.Info("blkdiscard worker finished", "path", path)
 		return err
@@ -58,7 +68,7 @@ func (m *LinearBlobManager) blkdiscardWorkName(name string) string {
 }
 
 // Create or expand a blob.
-func (m *LinearBlobManager) CreateBlob(ctx context.Context, name string, sizeBytes int64, owner client.Object) error {
+func (m *LinearBlobManager) CreateBlob(ctx context.Context, name string, sizeBytes int64, skipWipe bool, owner client.Object) error {
 	// This creates but does not resize.
 	_, err := commands.LvmLvCreateIdempotent(
 		"--devicesfile", m.vgName,
@@ -98,9 +108,10 @@ func (m *LinearBlobManager) CreateBlob(ctx context.Context, name string, sizeByt
 		}
 
 		work := &blkdiscardWork{
-			vgName: m.vgName,
-			lvName: name,
-			offset: offset,
+			vgName:   m.vgName,
+			lvName:   name,
+			offset:   offset,
+			skipWipe: skipWipe,
 		}
 		err := m.workers.Run(m.blkdiscardWorkName(name), owner, work)
 		if err != nil {

api/v1alpha1/volume_types.go
@@ -29,6 +29,13 @@ type VolumeSpec struct {
 
 	// Should be set from creation and never updated.
 	// +kubebuilder:validation:XValidation:rule=oldSelf==self
+	// +kubebuilder:validation:Enum=Full;UnsafeFast
+	// +default:value="Full"
+	// +optional
+	WipePolicy VolumeWipePolicy `json:"wipePolicy,omitempty"`
+
+	// Should be set from creation and never updated.
+	// +kubebuilder:validation:XValidation:rule=oldSelf==self
 	// +required
 	Type VolumeType `json:"type"`
 
@@ -74,10 +81,14 @@ func (v *VolumeSpec) ReadOnly() bool {
 }
 
 type VolumeMode string
+type VolumeWipePolicy string
 
 const (
 	VolumeModeThin   VolumeMode = "Thin"
 	VolumeModeLinear VolumeMode = "Linear"
+
+	VolumeWipePolicyFull       VolumeWipePolicy = "Full"
+	VolumeWipePolicyUnsafeFast VolumeWipePolicy = "UnsafeFast"
 )
 
 // + TODO make this a proper discriminated union

deploy/kubernetes/crd/kubesan.gitlab.io_volumes.yaml
@@ -177,6 +177,15 @@ spec:
                 type: string
                 x-kubernetes-validations:
                 - rule: oldSelf==self
+              wipePolicy:
+                default: Full
+                description: Should be set from creation and never updated.
+                enum:
+                - Full
+                - UnsafeFast
+                type: string
+                x-kubernetes-validations:
+                - rule: oldSelf==self
             required:
             - accessModes
             - contents

internal/csi/controller/volume.go
@@ -72,6 +72,11 @@ func (s *ControllerServer) CreateVolume(ctx context.Context, req *csi.CreateVolu
 		return nil, err
 	}
 
+	wipePolicy, err := getWipePolicy(req.Parameters)
+	if err != nil {
+		return nil, err
+	}
+
 	volumeType, err := getVolumeType(req.VolumeCapabilities)
 	if err != nil {
 		return nil, err
@@ -117,6 +122,7 @@ func (s *ControllerServer) CreateVolume(ctx context.Context, req *csi.CreateVolu
 		Spec: v1alpha1.VolumeSpec{
 			VgName:      lvmVolumeGroup,
 			Mode:        volumeMode,
+			WipePolicy:  wipePolicy,
 			Type:        *volumeType,
 			Contents:    *volumeContents,
 			AccessModes: accessModes,
@@ -168,6 +174,18 @@ func getVolumeMode(parameters map[string]string) (v1alpha1.VolumeMode, error) {
 	}
 
 	return v1alpha1.VolumeMode(mode), nil
+}
+
+func getWipePolicy(parameters map[string]string) (v1alpha1.VolumeWipePolicy, error) {
+	switch policy := parameters["wipePolicy"]; policy {
+	case "":
+		fallthrough
+	case string(v1alpha1.VolumeWipePolicyFull):
+		return v1alpha1.VolumeWipePolicyFull, nil
+	case string(v1alpha1.VolumeWipePolicyUnsafeFast):
+		return v1alpha1.VolumeWipePolicyUnsafeFast, nil
+	}
+	return "", status.Error(codes.InvalidArgument, "invalid volume wipe policy")
 }
 
 func getVolumeType(capabilities []*csi.VolumeCapability) (*v1alpha1.VolumeType, error) {
@@ -503,6 +521,10 @@ func validateVolume(volume *v1alpha1.Volume, size int64, limit int64, source *v1
 		return "incompatible parameter \"mode\""
 	}
 
+	if wipePolicy, err := getWipePolicy(parameters); err != nil || wipePolicy != volume.Spec.WipePolicy {
+		return "incompatible parameter \"wipePolicy\""
+	}
+
 	return validateCapabilities(volume, capabilities)
 }
 
@@ -560,7 +582,7 @@ func (s *ControllerServer) ValidateVolumeCapabilities(ctx context.Context, req *
 func copyKnownParameters(parameters map[string]string) map[string]string {
 	result := make(map[string]string, len(parameters))
 	for key, value := range parameters {
-		if key == "lvmVolumeGroup" || key == "mode" {
+		if key == "lvmVolumeGroup" || key == "mode" || key == "wipePolicy" {
 			result[key] = value
 		}
 	}

internal/manager/cluster/volume_controller.go
@@ -259,7 +259,8 @@ func (r *VolumeReconciler) reconcileNotDeleting(ctx context.Context, blobMgr blo
 	// create or expand LVM LV if necessary
 
 	if !meta.IsStatusConditionTrue(volume.Status.Conditions, v1alpha1.VolumeConditionLvCreated) || needsResize {
-		if err := blobMgr.CreateBlob(ctx, volume.Name, volume.Spec.SizeBytes, volume); err != nil {
+		skipWipe := volume.Spec.Type.Filesystem != nil || volume.Spec.WipePolicy == v1alpha1.VolumeWipePolicyUnsafeFast
+		if err := blobMgr.CreateBlob(ctx, volume.Name, volume.Spec.SizeBytes, skipWipe, volume); err != nil {
 			return err
 		}
 

internal/manager/common/blobs/blob_manager.go
@@ -17,9 +17,13 @@ type BlobManager interface {
 	// If the blob already exists but the size does not match then it will
 	// be expanded to the desired size.
 	//
+	// If skipWipe is true, it is not necessary to wipe the entire blob;
+	// however, the first 4Mi must read as zero on creation, to ensure
+	// any filesystem can be safely installed.
+	//
 	// An owner reference may be added from the given owner to a dependent
 	// resource associated with the new blob.
-	CreateBlob(ctx context.Context, name string, sizeBytes int64, owner client.Object) error
+	CreateBlob(ctx context.Context, name string, sizeBytes int64, skipWipe bool, owner client.Object) error
 
 	// RemoveBlob removes a blob if it exists. No error is returned if the
 	// blob does not exist. An owner reference may be removed from the

internal/manager/common/blobs/blob_manager_thin.go
@@ -147,7 +147,7 @@ func (m *ThinBlobManager) forgetRemovedThinLv(ctx context.Context, thinPoolLv *v
 }
 
 // Create or expand a thin volume blob within the manager's pool.
-func (m *ThinBlobManager) CreateBlob(ctx context.Context, name string, sizeBytes int64, owner client.Object) error {
+func (m *ThinBlobManager) CreateBlob(ctx context.Context, name string, sizeBytes int64, skipWipe bool, owner client.Object) error {
 	log := log.FromContext(ctx).WithValues("blobName", name, "nodeName", config.LocalNodeName)
 
 	thinPoolLv, err := m.createThinPoolLv(ctx, name, sizeBytes, owner)

commit ba374c8d6967426dfed6715b7d88b1ef1bcf03d8

csi: Fix blkdiscard usage
It turns out that blkdiscard understands only 3 suffix formats: 4MB
for power of 10, and 4M or 4MiB for power of 2 (unlike k8s Quantity
that also accepts 4Mi), which meant fast wipes weren't even wiping the
first extent of linear volumes, like they were supposed to do.  And at
any rate, spelling it out in bytes is consistent with what we did for
--offset.

Fixes: 138b0bd7 ("csi: Add new wipePolicy:UnsafeFast parameter to StorageClass")
Signed-off-by: Eric Blake <eblake@redhat.com>

internal/manager/common/blobs/blob_manager_linear.go
@@ -51,7 +51,7 @@ func (w *blkdiscardWork) Run(ctx context.Context) error {
 		args := []string{"blkdiscard", "--zeroout", "--offset",
 			strconv.FormatInt(w.offset, 10)}
 		if w.skipWipe {
-			args = append(args, "--length", "4Mi")
+			args = append(args, "--length", strconv.Itoa(4*1024*1024))
 		}
 		args = append(args, path)
 		log.Info("blkdiscard worker zeroing LV", "path", path, "command", args)