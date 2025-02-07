kubesan/kubesan

commit b20c872b83d44b544d3a9ed69debbbd6bb9574e3

thinpool: Update oldThinPoolLv on client.Update()
We have a couple of places where a reconciler makes more than one call
to UpdateThinPoolLv().  If the first call makes any change to state,
then we should update oldThinPoolLv to that state so that the caller
does not have to redo client.Get, yet the second call will still be
making the correct comparison on whether a change is needed.

Signed-off-by: Eric Blake <eblake@redhat.com>

internal/manager/common/thinpoollv/thinpoollv.go
@@ -159,6 +159,9 @@ func UpdateThinPoolLv(ctx context.Context, client client.Client, oldThinPoolLv, 
 		return nil
 	}
 	log.Info("Updating ThinPoolLv", "former", former, "preferred", preferred, "Spec.ActiveOnNode", thinPoolLv.Spec.ActiveOnNode, "Status.ActiveOnNode", thinPoolLv.Status.ActiveOnNode)
+	if oldThinPoolLv != nil {
+		thinPoolLv.DeepCopyInto(oldThinPoolLv)
+	}
 	return client.Update(ctx, thinPoolLv)
 }
 

commit 7a89712a066d04d24cdf3107d962eb52ac9b7746

thinpool: Make copy after successful Update
It turns out that client.Update not only writes, but reads back what
the server may have adjusted things to based on mutating webhooks.
Among other things, this means that the metadata volume.Generation
number is different after Update than it was before - and we WANT that
newer generation number to be the basis of any further edits in the
same control path.

Fixes: b20c872b8 ("thinpool: Update oldThinPoolLv on client.Update()", v0.10.0)
Signed-off-by: Eric Blake <eblake@redhat.com>

internal/manager/common/thinpoollv/thinpoollv.go
@@ -159,10 +159,11 @@ func UpdateThinPoolLv(ctx context.Context, client client.Client, oldThinPoolLv, 
 		return nil
 	}
 	log.Info("Updating ThinPoolLv", "former", former, "preferred", preferred, "Spec.ActiveOnNode", thinPoolLv.Spec.ActiveOnNode, "Status.ActiveOnNode", thinPoolLv.Status.ActiveOnNode)
-	if oldThinPoolLv != nil {
+	err := client.Update(ctx, thinPoolLv)
+	if err == nil && oldThinPoolLv != nil {
 		thinPoolLv.DeepCopyInto(oldThinPoolLv)
 	}
-	return client.Update(ctx, thinPoolLv)
+	return err
 }
 
 func ActivateThinLv(ctx context.Context, client client.Client, oldThinPoolLv, thinPoolLv *v1alpha1.ThinPoolLv, name string) error {