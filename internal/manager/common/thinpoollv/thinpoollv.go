kubesan/kubesan

commit a5647fbf2a054121ca7a0a657670bc9cbd90c132

manager: Implement cloning from snapshots and volumes
The Volume cluster controller uses the new BlobManager cloning API to
activate source and target blobs on a node.

The Volume node controller then runs a dd(1) worker to perform the data
copy.

Afterwards the Volume cluster controller deactivates the blobs and sets
the Available condition on the new Volume.

Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

internal/manager/node/volume_node_controller.go
@@ -26,24 +26,28 @@ import (
 	kubesanslices "gitlab.com/kubesan/kubesan/internal/common/slices"
 	"gitlab.com/kubesan/kubesan/internal/manager/common/thinpoollv"
 	"gitlab.com/kubesan/kubesan/internal/manager/common/util"
+	"gitlab.com/kubesan/kubesan/internal/manager/common/workers"
 )
 
 type VolumeNodeReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
+	Scheme  *runtime.Scheme
+	workers *workers.Workers
 }
 
 func SetUpVolumeNodeReconciler(mgr ctrl.Manager) error {
 	r := &VolumeNodeReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
+		Client:  mgr.GetClient(),
+		Scheme:  mgr.GetScheme(),
+		workers: workers.NewWorkers(),
 	}
 
-	return ctrl.NewControllerManagedBy(mgr).
+	b := ctrl.NewControllerManagedBy(mgr).
 		For(&v1alpha1.Volume{}).
 		Owns(&v1alpha1.ThinPoolLv{}, builder.MatchEveryOwner). // for ThinBlobManager
-		Owns(&v1alpha1.NBDExport{}).
-		Complete(r)
+		Owns(&v1alpha1.NBDExport{})
+	r.workers.SetUpReconciler(b)
+	return b.Complete(r)
 }
 
 // +kubebuilder:rbac:groups=kubesan.gitlab.io,resources=volumes,verbs=get;list;watch;create;update;patch;delete,namespace=kubesan-system
@@ -268,6 +272,82 @@ func devName(volume *v1alpha1.Volume) string {
 	return "/dev/" + volume.Spec.VgName + "/" + thinpoollv.VolumeToThinLvName(volume.Name)
 }
 
+type ddWork struct {
+	targetPathOnHost string
+	sourcePathOnHost string
+}
+
+func (w *ddWork) Run(ctx context.Context) error {
+	log := log.FromContext(ctx).WithValues("nodeName", config.LocalNodeName)
+
+	log.Info("Starting to populate new volume", "sourcePathOnHost", w.sourcePathOnHost, "targetPathOnHost", w.targetPathOnHost)
+	_, err := commands.RunOnHostContext(
+		ctx,
+		"dd",
+		"if="+w.sourcePathOnHost,
+		"of="+w.targetPathOnHost,
+		"bs=1M",
+		"conv=fsync,nocreat,sparse",
+	)
+	if err != nil {
+		log.Error(err, "dd failed")
+		return err
+	}
+
+	log.Info("Finished populating new volume", "sourcePathOnHost", w.sourcePathOnHost, "targetPathOnHost", w.targetPathOnHost)
+	return nil
+}
+
+func getSourcePathOnHost(volume *v1alpha1.Volume) string {
+	// Cross-VG cloning is not supported, so it's safe to use the target Volume's VG
+	vgName := volume.Spec.VgName
+	return "/dev/" + vgName + "/" + thinpoollv.VolumeToThinLvName("clone-"+volume.Name)
+}
+
+func (r *VolumeNodeReconciler) reconcileThinPopulating(ctx context.Context, volume *v1alpha1.Volume) error {
+	log := log.FromContext(ctx).WithValues("nodeName", config.LocalNodeName)
+
+	workName := "dd-" + volume.Name
+
+	if volume.DeletionTimestamp != nil {
+		log.Info("Canceling dd work due to volume deletion", "volumeName", volume.Name)
+		return r.workers.Cancel(workName) // stop dd
+	}
+
+	sourcePathOnHost := getSourcePathOnHost(volume)
+	targetPathOnHost := devName(volume)
+
+	// only run on node where data source is activated
+
+	if exists, _ := commands.PathExistsOnHost(sourcePathOnHost); !exists {
+		log.Info("Source path does not exist on host", "path", sourcePathOnHost)
+		return nil
+	}
+	if exists, _ := commands.PathExistsOnHost(targetPathOnHost); !exists {
+		log.Info("Target path does not exist on host", "path", targetPathOnHost)
+		return nil
+	}
+
+	work := &ddWork{
+		sourcePathOnHost: sourcePathOnHost,
+		targetPathOnHost: targetPathOnHost,
+	}
+	if err := r.workers.Run(workName, volume, work); err != nil {
+		return err
+	}
+
+	// signal to the cluster controller that data population is done
+
+	condition := metav1.Condition{
+		Type:    v1alpha1.VolumeConditionDataSourceCompleted,
+		Status:  metav1.ConditionTrue,
+		Reason:  "Completed",
+		Message: "population from data source completed",
+	}
+	meta.SetStatusCondition(&volume.Status.Conditions, condition)
+	return r.statusUpdate(ctx, volume)
+}
+
 func (r *VolumeNodeReconciler) reconcileThin(ctx context.Context, volume *v1alpha1.Volume) error {
 	thinPoolLv := &v1alpha1.ThinPoolLv{}
 
@@ -370,7 +450,27 @@ func (r *VolumeNodeReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
-	// check if already created
+	// perform data population
+
+	if meta.IsStatusConditionTrue(volume.Status.Conditions, v1alpha1.VolumeConditionLvCreated) &&
+		!meta.IsStatusConditionTrue(volume.Status.Conditions, v1alpha1.VolumeConditionDataSourceCompleted) {
+		var err error
+
+		switch volume.Spec.Mode {
+		case v1alpha1.VolumeModeThin:
+			err = r.reconcileThinPopulating(ctx, volume)
+		default:
+			err = errors.NewBadRequest("invalid volume mode for data population")
+		}
+
+		if _, ok := err.(*util.WatchPending); ok {
+			log.Info("reconcile waiting for Watch during data population")
+			return ctrl.Result{}, nil // wait until Watch triggers
+		}
+		return ctrl.Result{}, nil
+	}
+
+	// check if ready for operation
 
 	if !meta.IsStatusConditionTrue(volume.Status.Conditions, v1alpha1.VolumeConditionAvailable) {
 		return ctrl.Result{}, nil

internal/manager/cluster/snapshot_controller.go
@@ -43,7 +43,7 @@ func SetUpSnapshotReconciler(mgr ctrl.Manager) error {
 func (r *SnapshotReconciler) reconcileDeleting(ctx context.Context, blobMgr blobs.BlobManager, snapshot *v1alpha1.Snapshot) error {
 	log := log.FromContext(ctx).WithValues("nodeName", config.LocalNodeName)
 
-	if err := blobMgr.RemoveBlob(ctx, snapshot.Name); err != nil {
+	if err := blobMgr.RemoveBlob(ctx, snapshot.Name, snapshot); err != nil {
 		return err
 	}
 

internal/manager/cluster/volume_controller.go
@@ -9,6 +9,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -58,6 +59,118 @@ func (r *VolumeReconciler) newBlobManager(volume *v1alpha1.Volume) (blobs.BlobMa
 	}
 }
 
+// Returns the name of a temporary snapshot for volume cloning
+func cloneVolumeSnapshotName(volumeName string) string {
+	return "clone-" + volumeName
+}
+
+// Get a BlobManager and blob name for a given volume's data source
+func (r *VolumeReconciler) getDataSourceBlob(ctx context.Context, volume *v1alpha1.Volume) (blobs.BlobManager, string, error) {
+	switch volume.Spec.Contents.ContentsType {
+	case v1alpha1.VolumeContentsTypeCloneVolume:
+		name := volume.Spec.Contents.CloneVolume.SourceVolume
+
+		dataSrcVolume := &v1alpha1.Volume{}
+		if err := r.Client.Get(ctx, types.NamespacedName{Name: name, Namespace: config.Namespace}, dataSrcVolume); err != nil {
+			return nil, "", err
+		}
+
+		blobMgr, err := r.newBlobManager(dataSrcVolume)
+		if err != nil {
+			return nil, "", err
+		}
+		return blobMgr, name, nil
+	case v1alpha1.VolumeContentsTypeCloneSnapshot:
+		name := volume.Spec.Contents.CloneSnapshot.SourceSnapshot
+
+		dataSrcSnapshot := &v1alpha1.Snapshot{}
+		if err := r.Client.Get(ctx, types.NamespacedName{Name: name, Namespace: config.Namespace}, dataSrcSnapshot); err != nil {
+			return nil, "", err
+		}
+
+		blobMgr := blobs.NewThinBlobManager(r.Client, r.Scheme, dataSrcSnapshot.Spec.VgName, dataSrcSnapshot.Spec.SourceVolume)
+		return blobMgr, name, nil
+	default:
+		return nil, "", errors.NewBadRequest("cannot get data source for volume without contents")
+	}
+}
+
+func (r *VolumeReconciler) activateDataSource(ctx context.Context, blobMgr blobs.BlobManager, volume *v1alpha1.Volume) error {
+	log := log.FromContext(ctx).WithValues("nodeName", config.LocalNodeName)
+
+	if volume.Spec.Contents.ContentsType == v1alpha1.VolumeContentsTypeEmpty {
+		return nil // do nothing
+	}
+
+	dataSrcBlobMgr, dataSrcBlobName, err := r.getDataSourceBlob(ctx, volume)
+	if err != nil {
+		return err
+	}
+
+	// create a temporary snapshot to clone from
+
+	tmpBlobName := cloneVolumeSnapshotName(volume.Name)
+	if err := dataSrcBlobMgr.SnapshotBlob(ctx, tmpBlobName, dataSrcBlobName, volume); err != nil {
+		return err
+	}
+
+	log.Info("Temporary snapshot created for cloning", "tmpBlobName", tmpBlobName)
+
+	if err := dataSrcBlobMgr.ActivateBlobForCloneSource(ctx, tmpBlobName, volume); err != nil {
+		return err
+	}
+
+	log.Info("Activated source blob for cloning", "tmpBlobName", tmpBlobName)
+
+	if err := blobMgr.ActivateBlobForCloneTarget(ctx, volume.Name, dataSrcBlobMgr); err != nil {
+		return err
+	}
+
+	log.Info("Activated target blob for cloning", "volumeName", volume.Name)
+
+	return nil
+}
+
+func (r *VolumeReconciler) deactivateDataSource(ctx context.Context, blobMgr blobs.BlobManager, volume *v1alpha1.Volume) error {
+	log := log.FromContext(ctx).WithValues("nodeName", config.LocalNodeName)
+
+	if volume.Spec.Contents.ContentsType == v1alpha1.VolumeContentsTypeEmpty {
+		return nil // do nothing
+	}
+
+	if err := blobMgr.DeactivateBlobForCloneTarget(ctx, volume.Name); err != nil {
+		return err
+	}
+
+	log.Info("Deactivated target blob for cloning", "volumeName", volume.Name)
+
+	tmpBlobName := cloneVolumeSnapshotName(volume.Name)
+	dataSrcBlobMgr, _, err := r.getDataSourceBlob(ctx, volume)
+	if err == nil {
+		log.Info("About to deactivate source blob for cloning", "tmpBlobName", tmpBlobName)
+
+		if err := dataSrcBlobMgr.DeactivateBlobForCloneSource(ctx, tmpBlobName, volume); err != nil {
+			return err
+		}
+
+		log.Info("Deactivated source blob for cloning", "tmpBlobName", tmpBlobName)
+
+		// delete temporary snapshot
+
+		if err := dataSrcBlobMgr.RemoveBlob(ctx, tmpBlobName, volume); err != nil {
+			return err
+		}
+	} else if errors.IsNotFound(err) {
+		// already deleted
+	} else {
+		return err
+	}
+
+	log.Info("Removed temporary snapshot for cloning", "tmpBlobName", tmpBlobName)
+
+	return nil
+}
+
 func (r *VolumeReconciler) reconcileDeleting(ctx context.Context, blobMgr blobs.BlobManager, volume *v1alpha1.Volume) error {
 	log := log.FromContext(ctx).WithValues("nodeName", config.LocalNodeName)
 
@@ -66,7 +179,11 @@ func (r *VolumeReconciler) reconcileDeleting(ctx context.Context, blobMgr blobs.
 		return nil // wait until no longer attached
 	}
 
-	if err := blobMgr.RemoveBlob(ctx, volume.Name); err != nil {
+	if err := r.deactivateDataSource(ctx, blobMgr, volume); err != nil {
+		return err
+	}
+
+	if err := blobMgr.RemoveBlob(ctx, volume.Name, volume); err != nil {
 		// During deletion, if there were multiple
 		// OwnerReferences, we have no guarantee when
 		// kubernetes will remove our reference.  But once our
@@ -108,6 +225,12 @@ func (r *VolumeReconciler) reconcileNotDeleting(ctx context.Context, blobMgr blo
 
 		log.Info("CreateBlob succeeded")
 
+		if err := r.activateDataSource(ctx, blobMgr, volume); err != nil {
+			return err
+		}
+
+		log.Info("activateDataSource succeeded")
+
 		condition := metav1.Condition{
 			Type:    v1alpha1.VolumeConditionLvCreated,
 			Status:  metav1.ConditionTrue,
@@ -133,6 +256,10 @@ func (r *VolumeReconciler) reconcileNotDeleting(ctx context.Context, blobMgr blo
 
 	if !meta.IsStatusConditionTrue(volume.Status.Conditions, v1alpha1.VolumeConditionAvailable) &&
 		meta.IsStatusConditionTrue(volume.Status.Conditions, v1alpha1.VolumeConditionDataSourceCompleted) {
+		if err := r.deactivateDataSource(ctx, blobMgr, volume); err != nil {
+			return err
+		}
+
 		condition := metav1.Condition{
 			Type:    v1alpha1.VolumeConditionAvailable,
 			Status:  metav1.ConditionTrue,
@@ -208,17 +335,6 @@ func (r *VolumeReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 		if volume.Spec.Contents.CloneSnapshot == nil || volume.Spec.Contents.CloneVolume != nil {
 			return ctrl.Result{}, errors.NewBadRequest("invalid volume contents")
 		}
-	}
-
-	switch volume.Spec.Contents.ContentsType {
-	case v1alpha1.VolumeContentsTypeEmpty:
-		// nothing to do
-
-	case v1alpha1.VolumeContentsTypeCloneVolume:
-		return ctrl.Result{}, errors.NewBadRequest("cloning volumes is not yet supported")
-
-	case v1alpha1.VolumeContentsTypeCloneSnapshot:
-		return ctrl.Result{}, errors.NewBadRequest("cloning snapshots is not yet supported")
 	}
 
 	err = r.reconcileNotDeleting(ctx, blobMgr, volume)

internal/manager/common/blobs/blob_manager.go
@@ -22,8 +22,9 @@ type BlobManager interface {
 	CreateBlob(ctx context.Context, name string, sizeBytes int64, owner client.Object) error
 
 	// RemoveBlob removes a blob if it exists. No error is returned if the
-	// blob does not exist.
-	RemoveBlob(ctx context.Context, name string) error
+	// blob does not exist. An owner reference may be removed from the
+	// given owner to a dependent resource associated with the blob.
+	RemoveBlob(ctx context.Context, name string, owner client.Object) error
 
 	// SnapshotBlob creates a snapshot with a given name from an existing
 	// source blob.  If snapshots are not supported, this will fail.

internal/manager/common/blobs/blob_manager_linear.go
@@ -100,7 +100,7 @@ func (m *LinearBlobManager) CreateBlob(ctx context.Context, name string, sizeByt
 	return nil
 }
 
-func (m *LinearBlobManager) RemoveBlob(ctx context.Context, name string) error {
+func (m *LinearBlobManager) RemoveBlob(ctx context.Context, name string, owner client.Object) error {
 	// stop blkdiscard in case it's running
 	if err := m.workers.Cancel(m.blkdiscardWorkName(name)); err != nil {
 		return err

internal/manager/common/blobs/blob_manager_thin.go
@@ -216,14 +216,10 @@ func (m *ThinBlobManager) GetSize(ctx context.Context, name string) (int64, erro
 func (m *ThinBlobManager) SnapshotBlob(ctx context.Context, name string, sourceName string, owner client.Object) error {
 	log := log.FromContext(ctx).WithValues("blobName", name, "nodeName", config.LocalNodeName)
 
-	if sourceName != m.poolName {
-		return errors.NewBadRequest("source name must match blob manager pool name")
-	}
-
 	log.Info("SnapshotBlob entered", "name", name)
 	defer log.Info("SnapshotBlob exited")
 
-	thinPoolLv, err := m.getThinPoolLv(ctx, sourceName)
+	thinPoolLv, err := m.getThinPoolLv(ctx, m.poolName)
 	if err != nil {
 		log.Error(err, "SnapshotBlob getThinPoolLv failed")
 		return err
@@ -268,7 +264,7 @@ func (m *ThinBlobManager) SnapshotBlob(ctx context.Context, name string, sourceN
 	return nil
 }
 
-func (m *ThinBlobManager) RemoveBlob(ctx context.Context, name string) error {
+func (m *ThinBlobManager) RemoveBlob(ctx context.Context, name string, owner client.Object) error {
 	log := log.FromContext(ctx).WithValues("blobName", name, "thinPoolLvName", m.poolName, "nodeName", config.LocalNodeName)
 
 	thinPoolLv, err := m.getThinPoolLv(ctx, m.poolName)
@@ -300,6 +296,14 @@ func (m *ThinBlobManager) RemoveBlob(ctx context.Context, name string) error {
 	if thinPoolLv.Status.FindThinLv(thinLvName) != nil {
 		return util.NewWatchPending("waiting for thinlv cleanup")
 	}
+
+	// Normally when a Volume or Snapshot is deleted k8s automatically
+	// removes the owner reference, but cloning creates temporary snapshot
+	// LVs that are removed when cloning completes and the target Volume
+	// still lives for a long time. Try to remove the owner reference here
+	// but ignore errors in case k8s already removed it.
+
+	_ = controllerutil.RemoveOwnerReference(owner, thinPoolLv, m.scheme)
 
 	// update thinPoolLv to clear Spec.ActiveOnNode, if necessary
 

commit 4123c4023de6e12bd34c57a15384a03f2f961d44

clone: Don't lose cloning errors
If reconcileThinPopulating fails for other than WatchPending, we
should return that error to allow exponential backoff.  Also, we
should log the reason contained in WatchPending.

Fixes: a5647fbf ("manager: Implement cloning from snapshots and volumes")
Signed-off-by: Eric Blake <eblake@redhat.com>

internal/manager/common/thinpoollv/thinpoollv.go
@@ -183,7 +183,7 @@ func ActivateThinLv(ctx context.Context, client client.Client, oldThinPoolLv, th
 
 	thinLvStatus := thinPoolLv.Status.FindThinLv(thinLvName)
 	if thinLvStatus == nil || thinLvStatus.State.Name != v1alpha1.ThinLvStatusStateNameActive {
-		return &util.WatchPending{}
+		return util.NewWatchPending("waiting for ThinLv activation")
 	}
 	return nil
 }
@@ -212,7 +212,7 @@ func DeactivateThinLv(ctx context.Context, client client.Client, oldThinPoolLv, 
 
 	thinLvStatus := thinPoolLv.Status.FindThinLv(thinLvName)
 	if thinLvStatus != nil && thinLvStatus.State.Name == v1alpha1.ThinLvStatusStateNameActive {
-		return &util.WatchPending{}
+		return util.NewWatchPending("waiting for ThinLv deactivation")
 	}
 	return nil
 }

internal/manager/node/volume_node_controller.go
@@ -491,11 +491,11 @@ func (r *VolumeNodeReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 			err = errors.NewBadRequest("invalid volume mode for data population")
 		}
 
-		if _, ok := err.(*util.WatchPending); ok {
-			log.Info("reconcile waiting for Watch during data population")
+		if watch, ok := err.(*util.WatchPending); ok {
+			log.Info("reconcile waiting for Watch during data population", "why", watch.Why)
 			return ctrl.Result{}, nil // wait until Watch triggers
 		}
-		return ctrl.Result{}, nil
+		return ctrl.Result{}, err
 	}
 
 	// check if ready for operation