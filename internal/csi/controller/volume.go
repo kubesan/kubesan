kubesan/kubesan

commit 066ce91b95af5a266ccf8aad45c647ecee4d8500

csi: Fast-fail validations of creation from contents
Reject things that we know are impossible or that we don't support
yet, rather than waiting around forever for the manager to make
progress where it cannot do so.  In some cases, it's better to be
strict now where we can relax things later (for example, allowing
cross-VG clones even if we have to use dd may be desirable, as doing
it with a single CSI driver is still easier for the user than having
them spin up an external solution). But even there, once we start
supporting node topologies where VGs are only on a subset of nodes, we
have to be careful to only support clones when both source and
destination can be staged on the same node.

Note: We may decide to do all these validations only in the manager
layer, and instead rely on Conditions["Error"] to let us report those
failures at the gRPC layer.  But for now this was a simple patch that
makes it easier to ensure creation from contents doesn't hang
csi-sanity.

Signed-off-by: Eric Blake <eblake@redhat.com>

internal/csi/controller/volume.go
@@ -74,17 +74,17 @@ func (s *ControllerServer) CreateVolume(ctx context.Context, req *csi.CreateVolu
 		return nil, err
 	}
 
-	volumeContents, err := getVolumeContents(req)
-	if err != nil {
-		return nil, err
-	}
-
 	accessModes, err := getVolumeAccessModes(req.VolumeCapabilities)
 	if err != nil {
 		return nil, err
 	}
 
 	capacity, limit, err := validateCapacity(req.CapacityRange, defaultExtentSize)
+	if err != nil {
+		return nil, err
+	}
+
+	volumeContents, err := s.getVolumeContents(ctx, req, lvmVolumeGroup, limit, volumeType)
 	if err != nil {
 		return nil, err
 	}
@@ -207,27 +207,96 @@ func getVolumeType(capabilities []*csi.VolumeCapability) (*v1alpha1.VolumeType, 
 	return volumeType, nil
 }
 
-func getVolumeContents(req *csi.CreateVolumeRequest) (*v1alpha1.VolumeContents, error) {
+// Determine what to populate the volume with, and ensure that any source
+// exists in vgName, is not larger than any non-zero limit, and that we
+// are not attempting to convert a block into a filesystem.
+func (s *ControllerServer) getVolumeContents(ctx context.Context, req *csi.CreateVolumeRequest, vgName string, limit int64, volumeType *v1alpha1.VolumeType) (*v1alpha1.VolumeContents, error) {
 	volumeContents := &v1alpha1.VolumeContents{}
 
 	if req.VolumeContentSource == nil {
+		// Empty source
 		volumeContents.ContentsType = v1alpha1.VolumeContentsTypeEmpty
+
 	} else if source := req.VolumeContentSource.GetVolume(); source != nil {
-		if _, err := validate.ValidateVolumeID(source.VolumeId); err != nil {
+		// Volume source
+		name, err := validate.ValidateVolumeID(source.VolumeId)
+		if err != nil {
 			return nil, err
+		}
+		volume := &v1alpha1.Volume{}
+		err = s.client.Get(ctx, name, volume)
+		// TODO: Once we get Conditions["Error"] reporting working,
+		// it may be nicer to do these checks only in the manager
+		// code, instead of duplicating them here as fail-fast checks.
+		switch {
+		case errors.IsNotFound(err):
+			return nil, status.Error(codes.NotFound, "source volume does not exist")
+
+		case err != nil:
+			return nil, status.Errorf(codes.InvalidArgument, "unable to inspect source volume: %v", err)
+
+		case volume.Spec.VgName != vgName:
+			// TODO We could possibly relax this for multiple VGs that share a common node in topology
+			return nil, status.Error(codes.InvalidArgument, "source volume does not live in same volume group as destination")
+
+		case volumeType.Filesystem != nil:
+			return nil, status.Error(codes.InvalidArgument, "cannot create filesystem volume from a block device source")
+
+		case volume.Spec.Type.Filesystem != nil:
+			// TODO For now, we simply refuse to support cloning
+			// a block from a filesystem. It might be possible to
+			// support this in the future, though.
+			return nil, status.Error(codes.InvalidArgument, "cannot create block device from a filesystem source")
+
+		case volume.Spec.Mode == v1alpha1.VolumeModeLinear:
+			// Cloning a linear volume is not possible.
+			return nil, status.Error(codes.InvalidArgument, "cannot clone a linear volume")
+
+		case !meta.IsStatusConditionTrue(volume.Status.Conditions, v1alpha1.VolumeConditionAvailable):
+			return nil, status.Error(codes.NotFound, "source volume is not ready yet")
+
+		case limit > 0 && volume.Status.SizeBytes > limit:
+			return nil, status.Errorf(codes.OutOfRange, "destination capacity is insufficient to accommodate source size %d", volume.Status.SizeBytes)
 		}
 		volumeContents.ContentsType = v1alpha1.VolumeContentsTypeCloneVolume
 		volumeContents.CloneVolume = &v1alpha1.VolumeContentsCloneVolume{
 			SourceVolume: source.VolumeId,
 		}
+
 	} else if source := req.VolumeContentSource.GetSnapshot(); source != nil {
-		if _, err := validate.ValidateSnapshotID(source.SnapshotId); err != nil {
+		// Snapshot source
+		name, err := validate.ValidateSnapshotID(source.SnapshotId)
+		if err != nil {
 			return nil, err
+		}
+		snapshot := &v1alpha1.Snapshot{}
+		err = s.client.Get(ctx, name, snapshot)
+		switch {
+		case errors.IsNotFound(err):
+			return nil, status.Error(codes.NotFound, "source snapshot does not exist")
+
+		case err != nil:
+			return nil, status.Errorf(codes.InvalidArgument, "unable to inspect source snapshot: %v", err)
+
+		case snapshot.Spec.VgName != vgName:
+			// TODO We could possibly relax this for multiple VGs that share a common node in topology
+			return nil, status.Error(codes.InvalidArgument, "source snapshot does not live in same volume group as destination")
+
+		case volumeType.Filesystem != nil:
+			// TODO This is true as long as Snapshots can only be created from block devices
+			return nil, status.Error(codes.InvalidArgument, "cannot create filesystem volume from a block device source")
+
+		case !meta.IsStatusConditionTrue(snapshot.Status.Conditions, v1alpha1.SnapshotConditionAvailable):
+			return nil, status.Error(codes.NotFound, "source snapshot is not ready yet")
+
+		case limit > 0 && snapshot.Status.SizeBytes > limit:
+			return nil, status.Errorf(codes.OutOfRange, "destination capacity is insufficient to accommodate source size %d", snapshot.Status.SizeBytes)
 		}
 		volumeContents.ContentsType = v1alpha1.VolumeContentsTypeCloneSnapshot
 		volumeContents.CloneSnapshot = &v1alpha1.VolumeContentsCloneSnapshot{
 			SourceSnapshot: source.SnapshotId,
 		}
+
 	} else {
 		return nil, status.Error(codes.InvalidArgument, "unsupported volume content source")
 	}

commit dad59da6a1f7d5600c30479a2e84e804aa949ba2

csi: Don't hang on attempt to clone into linear
While we may eventually support the creation of a pre-populated linear
volume, right now any attempt to do so hangs indefinitely (the Volume
never finishes creation, so the CSI call always fails with a timeout).
Better is to fail fast; missed in my earlier set of fail-fast checks.

Fixes: 066ce91b ("csi: Fast-fail validataions of creation from contents")
Signed-off-by: Eric Blake <eblake@redhat.com>

internal/csi/controller/volume.go
@@ -81,6 +81,9 @@ func (s *ControllerServer) CreateVolume(ctx context.Context, req *csi.CreateVolu
 	if err != nil {
 		return nil, err
 	}
+	if volumeContents.ContentsType != v1alpha1.VolumeContentsTypeEmpty && volumeMode != v1alpha1.VolumeModeThin {
+		return nil, status.Error(codes.Unimplemented, "cannot clone into a linear volume")
+	}
 
 	// We don't advertise MODIFY_VOLUME, so mutable parameters are unexpected.
 	if len(req.MutableParameters) > 0 {
@@ -267,7 +270,7 @@ func (s *ControllerServer) getVolumeContents(ctx context.Context, req *csi.Creat
 
 		case volume.Spec.Mode == v1alpha1.VolumeModeLinear:
 			// Cloning a linear volume is not possible.
-			return nil, status.Error(codes.InvalidArgument, "cannot clone a linear volume")
+			return nil, status.Error(codes.InvalidArgument, "cannot clone from a linear volume")
 
 		case !meta.IsStatusConditionTrue(volume.Status.Conditions, v1alpha1.VolumeConditionAvailable):
 			return nil, status.Error(codes.NotFound, "source volume is not ready yet")
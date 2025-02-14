dad59da6a1f7d5600c30479a2e84e804aa949ba2
Eric Blake <eblake@redhat.com>
2025-02-14 13:54:32 -0600

    csi: Don't hang on attempt to clone into linear
    While we may eventually support the creation of a pre-populated linear
    volume, right now any attempt to do so hangs indefinitely (the Volume
    never finishes creation, so the CSI call always fails with a timeout).
    Better is to fail fast; missed in my earlier set of fail-fast checks.
    
    Fixes: 066ce91b ("csi: Fast-fail validataions of creation from contents")
    Signed-off-by: Eric Blake <eblake@redhat.com>

--- internal/csi/controller/volume.go
+++ internal/csi/controller/volume.go
@@ -81,6 +81,9 @@ func (s *ControllerServer) CreateVolume(ctx context.Context, req *csi.CreateVolu
 	if err != nil {
 		return nil, err
 	}
+	if volumeContents.ContentsType != v1alpha1.VolumeContentsTypeEmpty && volumeMode != v1alpha1.VolumeModeThin {
+		return nil, status.Error(codes.Unimplemented, "cannot clone into a linear volume")
+	}
 
 	// We don't advertise MODIFY_VOLUME, so mutable parameters are unexpected.
 	if len(req.MutableParameters) > 0 {
@@ -267,7 +270,7 @@ func (s *ControllerServer) getVolumeContents(ctx context.Context, req *csi.Creat
 
 		case volume.Spec.Mode == v1alpha1.VolumeModeLinear:
 			// Cloning a linear volume is not possible.
-			return nil, status.Error(codes.InvalidArgument, "cannot clone a linear volume")
+			return nil, status.Error(codes.InvalidArgument, "cannot clone from a linear volume")
 
 		case !meta.IsStatusConditionTrue(volume.Status.Conditions, v1alpha1.VolumeConditionAvailable):
 			return nil, status.Error(codes.NotFound, "source volume is not ready yet")
