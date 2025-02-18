kubesan/kubesan

commit acc7b6ce0095e796f0498716c2f78714f3a665a6

volume: Query actual lvm size
Instead of blindly copying spec to status, we should grab actual size
from the lvm volume (Linear reads it directly in Volume, while Thin
has a layer of indirection through ThinPoolLv.Status.ThinLvs[]).  This
will become important in upcoming patches that perform volume
expansion - a resize isn't complete until we can read back the new
size from the device.

The placement of the size check after a successful CreateBlob() is
intentional - we should only update size exposed to CSI when
conditions["Available"] is true (the CSI spec says that a second
Create call while the first is still in progress must not change the
size, and we don't have to worry about odd interactions between a
blkdiscard process started at one size with an expansion to a
different size - but we do intend to use the documented CreateBlob
ability to resize a linear blob).

Similarly, when sizing device mapper wrappers, we should use the size
of the LV as it currently is, rather than a potentially larger
requested size that is still pending expansion.

Signed-off-by: Eric Blake <eblake@redhat.com>

internal/manager/node/volume_node_controller.go
@@ -56,16 +56,17 @@ func (r *VolumeNodeReconciler) reconcileThinAttaching(ctx context.Context, volum
 	oldThinPoolLv := thinPoolLv.DeepCopy()
 	log := log.FromContext(ctx).WithValues("nodeName", config.LocalNodeName)
 
-	if err := dm.Create(ctx, volume.Name, volume.Spec.SizeBytes); err != nil {
-		return err
-	}
-
 	thinLvName := thinpoollv.VolumeToThinLvName(volume.Name)
 	thinLvSpec := thinPoolLv.Spec.FindThinLv(thinLvName)
-	if thinLvSpec == nil || thinLvSpec.State.Name == v1alpha1.ThinLvSpecStateNameRemoved {
+	thinLvStatus := thinPoolLv.Status.FindThinLv(thinLvName)
+	if thinLvSpec == nil || thinLvStatus == nil || thinLvSpec.State.Name == v1alpha1.ThinLvSpecStateNameRemoved {
 		return errors.NewBadRequest("unexpected missing blob")
 	} else {
 		thinLvSpec.State.Name = v1alpha1.ThinLvSpecStateNameActive
+	}
+
+	if err := dm.Create(ctx, volume.Name, thinLvStatus.SizeBytes); err != nil {
+		return err
 	}
 
 	// Update the ThinPool activation claims.
@@ -102,7 +103,7 @@ func (r *VolumeNodeReconciler) reconcileThinAttaching(ctx context.Context, volum
 		}
 	}
 	log.Info("Device ready to attach", "device", device)
-	return dm.Resume(ctx, volume.Name, volume.Spec.SizeBytes, device)
+	return dm.Resume(ctx, volume.Name, thinLvStatus.SizeBytes, device)
 }
 
 // Ensure that the volume is detached from this node.

internal/common/commands/commands.go
@@ -11,6 +11,7 @@ import (
 	"os/exec"
 	"path"
 	"regexp"
+	"strconv"
 	"strings"
 
 	k8serror "k8s.io/apimachinery/pkg/api/errors"
@@ -214,6 +215,23 @@ func LvmLvAddTag(vgName string, lvName string, tag string) error {
 		fmt.Sprintf("%s/%s", vgName, lvName),
 	)
 	return err
+}
+
+// Return the current size of the LV.
+func LvmSize(vgName string, lvName string) (int64, error) {
+	output, err := Lvm(
+		"lvs",
+		"--devicesfile", vgName,
+		"--options", "lv_size",
+		"--units", "b",
+		"--no-headings",
+		"--no-suffix",
+		vgName+"/"+lvName,
+	)
+	if err != nil {
+		return 0, err
+	}
+	return strconv.ParseInt(strings.TrimSpace(string(output.Combined)), 10, 64)
 }
 
 // Calls a function with an LV activated temporarily

internal/manager/cluster/volume_controller.go
@@ -88,6 +88,9 @@ func (r *VolumeReconciler) reconcileDeleting(ctx context.Context, blobMgr blobs.
 }
 
 func (r *VolumeReconciler) reconcileNotDeleting(ctx context.Context, blobMgr blobs.BlobManager, volume *v1alpha1.Volume) error {
+	log := log.FromContext(ctx).WithValues("nodeName", config.LocalNodeName)
+	needsUpdate := false
+
 	// add finalizer
 
 	if controllerutil.AddFinalizer(volume, config.Finalizer) {
@@ -99,8 +102,6 @@ func (r *VolumeReconciler) reconcileNotDeleting(ctx context.Context, blobMgr blo
 	// create LVM LV if necessary
 
 	if !meta.IsStatusConditionTrue(volume.Status.Conditions, v1alpha1.VolumeConditionAvailable) {
-		log := log.FromContext(ctx).WithValues("nodeName", config.LocalNodeName)
-
 		if err := blobMgr.CreateBlob(ctx, volume.Name, volume.Spec.SizeBytes, volume); err != nil {
 			return err
 		}
@@ -115,13 +116,20 @@ func (r *VolumeReconciler) reconcileNotDeleting(ctx context.Context, blobMgr blo
 		}
 		meta.SetStatusCondition(&volume.Status.Conditions, condition)
 
-		sizeBytes, err := blobMgr.GetSize(ctx, volume.Name)
-		if err != nil {
-			return err
-		}
-		volume.Status.SizeBytes = sizeBytes
 		volume.Status.Path = blobMgr.GetPath(volume.Name)
+		needsUpdate = true
+	}
 
+	sizeBytes, err := blobMgr.GetSize(ctx, volume.Name)
+	if err != nil {
+		return err
+	}
+	if sizeBytes > volume.Status.SizeBytes {
+		needsUpdate = true
+		volume.Status.SizeBytes = sizeBytes
+	}
+
+	if needsUpdate {
 		if err := r.statusUpdate(ctx, volume); err != nil {
 			return err
 		}

internal/manager/common/blobs/blob_manager_linear.go
@@ -118,8 +118,9 @@ func (m *LinearBlobManager) SnapshotBlob(ctx context.Context, name string, sourc
 	return errors.NewBadRequest("linear volumes do not support snapshots")
 }
 
+// Return the actual size of the blob.
 func (m *LinearBlobManager) GetSize(ctx context.Context, name string) (int64, error) {
-	return 0, errors.NewBadRequest("not implemented yet")
+	return commands.LvmSize(m.vgName, name)
 }
 
 func (m *LinearBlobManager) GetPath(name string) string {

internal/manager/node/thinpoollv_node_controller.go
@@ -237,7 +237,6 @@ func (r *ThinPoolLvNodeReconciler) reconcileThinLvDeletion(ctx context.Context, 
 				State: v1alpha1.ThinLvStatusState{
 					Name: v1alpha1.ThinLvStatusStateNameRemoved,
 				},
-				SizeBytes: thinLvSpec.SizeBytes,
 			}
 
 			thinPoolLv.Status.ThinLvs = append(thinPoolLv.Status.ThinLvs, thinLvStatus)
@@ -428,12 +427,17 @@ func (r *ThinPoolLvNodeReconciler) createThinLv(ctx context.Context, thinPoolLv 
 		return nil
 	}
 
+	size, err := commands.LvmSize(thinPoolLv.Spec.VgName, thinLvSpec.Name)
+	if err != nil {
+		return err
+	}
+
 	thinLvStatus := v1alpha1.ThinLvStatus{
 		Name: thinLvSpec.Name,
 		State: v1alpha1.ThinLvStatusState{
 			Name: v1alpha1.ThinLvStatusStateNameInactive,
 		},
-		SizeBytes: thinLvSpec.SizeBytes,
+		SizeBytes: size,
 	}
 
 	thinPoolLv.Status.ThinLvs = append(thinPoolLv.Status.ThinLvs, thinLvStatus)

commit 999d26324cc64f5274316b4b6352156b362df666

volume: Grab better size when attaching NBD
Trying to size a device-mapper larger than the underlying device is
not a good idea.  Although the code is designed to restart a larger
NBD server any time the underlying LV is expanded, there is a
possibility that we can hit a race window where the existing NBD
server is still serving a smaller size than what we read from the
ThinPoolLv.  In that case, we should resume the linear wrapper with
the smaller size.

Fixes: acc7b6ce ("volume: Query actual lvm size", v0.10.0)
Signed-off-by: Eric Blake <eblake@redhat.com>

internal/manager/node/volume_node_controller.go
@@ -71,7 +71,8 @@ func (r *VolumeNodeReconciler) reconcileThinAttaching(ctx context.Context, volum
 		thinLvSpec.State.Name = v1alpha1.ThinLvSpecStateNameActive
 	}
 
-	if err := dm.Create(ctx, volume.Name, thinLvStatus.SizeBytes); err != nil {
+	sizeBytes := thinLvStatus.SizeBytes
+	if err := dm.Create(ctx, volume.Name, sizeBytes); err != nil {
 		return err
 	}
 
@@ -107,9 +108,10 @@ func (r *VolumeNodeReconciler) reconcileThinAttaching(ctx context.Context, volum
 		if err != nil {
 			return err
 		}
+		sizeBytes = nbdExport.Spec.SizeBytes
 	}
 	log.Info("Device ready to attach", "device", device)
-	return dm.Resume(ctx, volume.Name, thinLvStatus.SizeBytes, device)
+	return dm.Resume(ctx, volume.Name, sizeBytes, device)
 }
 
 // Ensure that the volume is detached from this node.