kubesan/kubesan

commit b355aa9c5b5a615f436c3fa022b0a6e39232e513

linear: Set DataSourceCompleted in node reconciler
Performing blkdiscard as part of CreateBlob in the cluster reconciler
means that the existence of the LV is not advertised in Volume.Status
until the zeroing is complete.  But an upcoming patch wants to change
things to expose LVs to the CSI sidecar as soon as
Conditions["LvCreated"] is true; to do that, the blkdiscard needs to
happen in a separate stage, rather than holding up the CreateBlob
call.  What's more, there is no technical reason why a linear volume
can't be populated by dd from a snapshot source, but for that to
happen, it will be easiest to reuse much of the data population code
for thin volumes which already lives in the node reconciler.

This patch changes the setting of Conditions["DataSourceCompleted"]
for linear volumes out of the cluster controller and into the node
controller, as well as activating at that point (thanks to the label,
we can now track which node has activated, and once again get away
with -aey instead of the -asy needed by commit e89e54aa), although it
will not be until a later patch that the actual zeroing is also moved.
And thanks to the label, it is now easy add in a placeholder to handle
future linear actions and to move the setting of the Conditions to the
common part of the reconcile after either linear or thin has had their
turn at running background job(s).  The shortcut for skipping a trip
through the node reconciler for a thin volume with an empty source is
left in place.

Signed-off-by: Eric Blake <eblake@redhat.com>

internal/manager/cluster/volume_controller.go
@@ -93,39 +93,37 @@ func (r *VolumeReconciler) getDataSourceBlob(ctx context.Context, volume *v1alph
 		blobMgr := blobs.NewThinBlobManager(r.Client, r.Scheme, dataSrcSnapshot.Spec.VgName, dataSrcSnapshot.Spec.SourceVolume)
 		return blobMgr, name, dataSrcSnapshot.Spec.SourceVolume, nil
 	default:
-		return nil, "", "", errors.NewBadRequest("cannot get data source for volume without contents")
+		return nil, "", "", nil
 	}
 }
 
 func (r *VolumeReconciler) activateDataSource(ctx context.Context, blobMgr blobs.BlobManager, volume *v1alpha1.Volume) error {
 	log := log.FromContext(ctx).WithValues("nodeName", config.LocalNodeName)
 
-	if volume.Spec.Contents.ContentsType == v1alpha1.VolumeContentsTypeEmpty {
-		return nil // do nothing
-	}
-
 	dataSrcBlobMgr, dataSrcBlobName, dataSrcPool, err := r.getDataSourceBlob(ctx, volume)
 	if err != nil {
 		return err
 	}
 
-	// create a temporary snapshot to clone from
+	if dataSrcBlobMgr != nil {
+		// create a temporary snapshot to clone from
 
-	tmpBlobName := cloneVolumeSnapshotName(volume.Name)
-	if err := dataSrcBlobMgr.SnapshotBlob(ctx, tmpBlobName, "", dataSrcBlobName, volume); err != nil {
-		return err
+		tmpBlobName := cloneVolumeSnapshotName(volume.Name)
+		if err := dataSrcBlobMgr.SnapshotBlob(ctx, tmpBlobName, "", dataSrcBlobName, volume); err != nil {
+			return err
+		}
+
+		log.Info("Temporary snapshot created for cloning", "tmpBlobName", tmpBlobName)
+
+		if err := dataSrcBlobMgr.ActivateBlobForCloneSource(ctx, tmpBlobName, volume); err != nil {
+			return err
+		}
+
+		log.Info("Activated source blob for cloning", "tmpBlobName", tmpBlobName)
 	}
-
-	log.Info("Temporary snapshot created for cloning", "tmpBlobName", tmpBlobName)
-
-	if err := dataSrcBlobMgr.ActivateBlobForCloneSource(ctx, tmpBlobName, volume); err != nil {
-		return err
-	}
-
-	log.Info("Activated source blob for cloning", "tmpBlobName", tmpBlobName)
 
 	node, err := blobMgr.ActivateBlobForCloneTarget(ctx, volume.Name, dataSrcBlobMgr)
-	if err != nil {
+	if err != nil || node == "" {
 		return err
 	}
 
@@ -140,7 +138,9 @@ func (r *VolumeReconciler) activateDataSource(ctx context.Context, blobMgr blobs
 	}
 	if _, exists := labels[config.PopulationNodeLabel]; !exists {
 		labels[config.PopulationNodeLabel] = node
-		labels[config.CloneSourceLabel] = dataSrcPool
+		if dataSrcPool != "" {
+			labels[config.CloneSourceLabel] = dataSrcPool
+		}
 		volume.SetLabels(labels)
 		if err := r.Update(ctx, volume); err != nil {
 			return err
@@ -153,10 +153,6 @@ func (r *VolumeReconciler) activateDataSource(ctx context.Context, blobMgr blobs
 
 func (r *VolumeReconciler) deactivateDataSource(ctx context.Context, blobMgr blobs.BlobManager, volume *v1alpha1.Volume) error {
 	log := log.FromContext(ctx).WithValues("nodeName", config.LocalNodeName)
-
-	if volume.Spec.Contents.ContentsType == v1alpha1.VolumeContentsTypeEmpty {
-		return nil // do nothing
-	}
 
 	if err := blobMgr.DeactivateBlobForCloneTarget(ctx, volume.Name); err != nil {
 		return err
@@ -257,15 +253,19 @@ func (r *VolumeReconciler) reconcilePrepareForResize(ctx context.Context, blobMg
 			if online {
 				return util.NewWatchPending("waiting for volume to be offline")
 			}
-			condition := metav1.Condition{
+			condition1 := metav1.Condition{
+				Type:    v1alpha1.VolumeConditionDataSourceCompleted,
+				Status:  metav1.ConditionFalse,
+				Reason:  "Resizing",
+				Message: "volume is unavailable while resizing",
+			}
+			condition2 := metav1.Condition{
 				Type:    v1alpha1.VolumeConditionAvailable,
 				Status:  metav1.ConditionFalse,
 				Reason:  "Resizing",
 				Message: "volume is unavailable while resizing",
 			}
-			// This update must happen now, rather than
-			// deferred to the end of reconcile.
-			if meta.SetStatusCondition(&volume.Status.Conditions, condition) {
+			if meta.SetStatusCondition(&volume.Status.Conditions, condition1) || meta.SetStatusCondition(&volume.Status.Conditions, condition2) {
 				needsUpdate = true
 			}
 		}
@@ -348,16 +348,24 @@ func (r *VolumeReconciler) reconcileNotDeleting(ctx context.Context, blobMgr blo
 		log.Info("activateDataSource succeeded")
 
 		// Shortcut when data population is not required
-		if volume.Spec.Contents.ContentsType == v1alpha1.VolumeContentsTypeEmpty {
-			condition := metav1.Condition{
+		var condition metav1.Condition
+		if volume.Spec.Contents.ContentsType == v1alpha1.VolumeContentsTypeEmpty && !blobMgr.ExpansionMustBeOffline() {
+			condition = metav1.Condition{
 				Type:    v1alpha1.VolumeConditionDataSourceCompleted,
 				Status:  metav1.ConditionTrue,
 				Reason:  "Completed",
 				Message: "population from data source completed",
 			}
-			if meta.SetStatusCondition(&volume.Status.Conditions, condition) {
-				needsUpdate = true
+		} else if !meta.IsStatusConditionTrue(volume.Status.Conditions, v1alpha1.VolumeConditionAvailable) {
+			condition = metav1.Condition{
+				Type:    v1alpha1.VolumeConditionAbnormal,
+				Status:  metav1.ConditionTrue,
+				Reason:  "Populating",
+				Message: "volume unavailable during initial population or expansion",
 			}
+		}
+		if meta.SetStatusCondition(&volume.Status.Conditions, condition) {
+			needsUpdate = true
 		}
 	}
 
@@ -370,6 +378,13 @@ func (r *VolumeReconciler) reconcileNotDeleting(ctx context.Context, blobMgr blo
 		condition := metav1.Condition{
 			Type:    v1alpha1.VolumeConditionAvailable,
 			Status:  metav1.ConditionTrue,
+			Reason:  "Available",
+			Message: "volume ready for use",
+		}
+		meta.SetStatusCondition(&volume.Status.Conditions, condition)
+		condition = metav1.Condition{
+			Type:    v1alpha1.VolumeConditionAbnormal,
+			Status:  metav1.ConditionFalse,
 			Reason:  "Available",
 			Message: "volume ready for use",
 		}

internal/manager/common/blobs/blob_manager_linear.go
@@ -147,23 +147,23 @@ func (m *LinearBlobManager) SnapshotBlob(ctx context.Context, name string, bindi
 }
 
 func (m *LinearBlobManager) ActivateBlobForCloneSource(ctx context.Context, name string, owner client.Object) error {
-	// Linear volumes do not support cloning
+	// Linear volumes do not support clone source
 	return errors.NewBadRequest("linear volumes do not support cloning")
 }
 
 func (m *LinearBlobManager) ActivateBlobForCloneTarget(ctx context.Context, name string, dataSrcBlobMgr BlobManager) (string, error) {
-	// Linear volumes do not support cloning
-	return "", errors.NewBadRequest("linear volumes do not support cloning")
+	// TODO Linear volumes could also support being a clone destination from a thin snapshot
+	return config.LocalNodeName, nil
 }
 
 func (m *LinearBlobManager) DeactivateBlobForCloneSource(ctx context.Context, name string, owner client.Object) error {
-	// Linear volumes do not support cloning
+	// Linear volumes do not support clone source
 	return errors.NewBadRequest("linear volumes do not support cloning")
 }
 
 func (m *LinearBlobManager) DeactivateBlobForCloneTarget(ctx context.Context, name string) error {
-	// Linear volumes do not support cloning
-	return errors.NewBadRequest("linear volumes do not support cloning")
+	// Deactivation was taken care of in the node reconciler.
+	return nil
 }
 
 // Return the actual size of the blob.

internal/manager/common/blobs/blob_manager_thin.go
@@ -334,6 +334,11 @@ func (m *ThinBlobManager) ActivateBlobForCloneSource(ctx context.Context, name s
 func (m *ThinBlobManager) ActivateBlobForCloneTarget(ctx context.Context, name string, dataSrcBlobMgr BlobManager) (string, error) {
 	log := log.FromContext(ctx).WithValues("blobName", name, "nodeName", config.LocalNodeName)
 
+	if dataSrcBlobMgr == nil {
+		// nothing to clone for empty source
+		return "", nil
+	}
+
 	thinPoolLv, err := m.getThinPoolLv(ctx, m.poolName)
 	if err != nil {
 		return "", err

internal/manager/node/volume_node_controller.go
@@ -322,7 +322,13 @@ func getSourcePathOnHost(volume *v1alpha1.Volume) string {
 	return "/dev/" + vgName + "/" + thinpoollv.VolumeToThinLvName("clone-"+volume.Name)
 }
 
+// Populate a Thin volume from a snapshot source.
 func (r *VolumeNodeReconciler) reconcileThinPopulating(ctx context.Context, volume *v1alpha1.Volume) error {
+	if volume.Spec.Contents.ContentsType == v1alpha1.VolumeContentsTypeEmpty {
+		// Empty volumes were done as no-op in cluster reconcile.
+		return errors.NewBadRequest("unexpected thin source")
+	}
+
 	log := log.FromContext(ctx).WithValues("nodeName", config.LocalNodeName)
 
 	workName := "dd-" + volume.Name
@@ -350,20 +356,37 @@ func (r *VolumeNodeReconciler) reconcileThinPopulating(ctx context.Context, volu
 		sourcePathOnHost: sourcePathOnHost,
 		targetPathOnHost: targetPathOnHost,
 	}
-	if err := r.workers.Run(workName, volume, work); err != nil {
+	return r.workers.Run(workName, volume, work)
+}
+
+// Populate a Linear volume from an empty source (that is, zero the image).
+func (r *VolumeNodeReconciler) reconcileLinearPopulating(ctx context.Context, volume *v1alpha1.Volume) error {
+	if volume.Spec.Contents.ContentsType != v1alpha1.VolumeContentsTypeEmpty {
+		// TODO allow linear to be populated from source
+		return errors.NewBadRequest("unexpected linear source")
+	}
+
+	log := log.FromContext(ctx).WithValues("nodeName", config.LocalNodeName)
+
+	// Activate so that the node reconciler can run blkdiscard.
+	_, err := commands.Lvm("lvchange",
+		"--devicesfile", volume.Spec.VgName,
+		"--activate", "ey",
+		volume.Spec.VgName+"/"+volume.Name)
+	if err != nil {
 		return err
 	}
 
-	// signal to the cluster controller that data population is done
+	targetPathOnHost := "/dev/" + volume.Spec.VgName + "/" + volume.Name
 
-	condition := metav1.Condition{
-		Type:    v1alpha1.VolumeConditionDataSourceCompleted,
-		Status:  metav1.ConditionTrue,
-		Reason:  "Completed",
-		Message: "population from data source completed",
-	}
-	meta.SetStatusCondition(&volume.Status.Conditions, condition)
-	return r.statusUpdate(ctx, volume)
+	// TODO Perform blkdiscard here, rather than in blob_manager_linear.
+	log.Info("perform block zero here instead of in cluster manager", "device", targetPathOnHost)
+
+	_, err = commands.Lvm("lvchange",
+		"--devicesfile", volume.Spec.VgName,
+		"--activate", "n",
+		volume.Spec.VgName+"/"+volume.Name)
+	return err
 }
 
 func (r *VolumeNodeReconciler) reconcileThin(ctx context.Context, volume *v1alpha1.Volume) error {
@@ -490,15 +513,33 @@ func (r *VolumeNodeReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 		switch volume.Spec.Mode {
 		case v1alpha1.VolumeModeThin:
 			err = r.reconcileThinPopulating(ctx, volume)
+		case v1alpha1.VolumeModeLinear:
+			err = r.reconcileLinearPopulating(ctx, volume)
 		default:
 			err = errors.NewBadRequest("invalid volume mode for data population")
 		}
 
-		if watch, ok := err.(*util.WatchPending); ok {
-			log.Info("reconcile waiting for Watch during data population", "why", watch.Why)
-			return ctrl.Result{}, nil // wait until Watch triggers
+		if err != nil || volume.DeletionTimestamp != nil {
+			if watch, ok := err.(*util.WatchPending); ok {
+				log.Info("reconcile waiting for Watch during data population", "why", watch.Why)
+				return ctrl.Result{}, nil // wait until Watch triggers
+			}
+			return ctrl.Result{}, err
 		}
-		return ctrl.Result{}, err
+
+		// signal to the cluster controller that data population is done
+
+		condition := metav1.Condition{
+			Type:    v1alpha1.VolumeConditionDataSourceCompleted,
+			Status:  metav1.ConditionTrue,
+			Reason:  "Completed",
+			Message: "population from data source completed",
+		}
+		if meta.SetStatusCondition(&volume.Status.Conditions, condition) {
+			if err := r.statusUpdate(ctx, volume); err != nil {
+				return ctrl.Result{}, err
+			}
+		}
 	}
 
 	// check if ready for operation

commit 94e975f3bc2e38ac7bfe947b4cc7ee7c2bf7b000

volume: Fix logic bug in linear cloning
If the code path has no condition to set, the command errors out due
to a CEL validation error on the zero struct.

Fixes: b355aa9c ("linear: Set DataSourceCompleted in node reconciler")
Signed-off-by: Eric Blake <eblake@redhat.com>

internal/manager/cluster/volume_controller.go
@@ -261,7 +261,15 @@ func (r *VolumeReconciler) reconcilePrepareForResize(ctx context.Context, blobMg
 				Reason:  "Resizing",
 				Message: "volume is unavailable while resizing",
 			}
-			if meta.SetStatusCondition(&volume.Status.Conditions, condition1) || meta.SetStatusCondition(&volume.Status.Conditions, condition2) {
+			condition3 := metav1.Condition{
+				Type:    v1alpha1.VolumeConditionAbnormal,
+				Status:  metav1.ConditionTrue,
+				Reason:  "Resizing",
+				Message: "volume unavailable during expansion",
+			}
+			if meta.SetStatusCondition(&volume.Status.Conditions, condition1) ||
+				meta.SetStatusCondition(&volume.Status.Conditions, condition2) ||
+				meta.SetStatusCondition(&volume.Status.Conditions, condition3) {
 				needsUpdate = true
 			}
 		}
@@ -343,24 +351,16 @@ func (r *VolumeReconciler) reconcileNotDeleting(ctx context.Context, blobMgr blo
 		log.Info("activateDataSource succeeded")
 
 		// Shortcut when data population is not required
-		var condition metav1.Condition
 		if volume.Spec.Contents.ContentsType == v1alpha1.VolumeContentsTypeEmpty && !blobMgr.ExpansionMustBeOffline() {
-			condition = metav1.Condition{
+			condition := metav1.Condition{
 				Type:    v1alpha1.VolumeConditionDataSourceCompleted,
 				Status:  metav1.ConditionTrue,
 				Reason:  "Completed",
 				Message: "population from data source completed",
 			}
-		} else if !meta.IsStatusConditionTrue(volume.Status.Conditions, v1alpha1.VolumeConditionAvailable) {
-			condition = metav1.Condition{
-				Type:    v1alpha1.VolumeConditionAbnormal,
-				Status:  metav1.ConditionTrue,
-				Reason:  "Populating",
-				Message: "volume unavailable during initial population or expansion",
+			if meta.SetStatusCondition(&volume.Status.Conditions, condition) {
+				needsUpdate = true
 			}
-		}
-		if meta.SetStatusCondition(&volume.Status.Conditions, condition) {
-			needsUpdate = true
 		}
 	}
 