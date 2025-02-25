kubesan/kubesan

commit bf91e85eeb96944f65258dab929233e99915c099

docs: Mention more lvm.conf tunables that we recommend
Dave Tiegland pointed out several settings that will improve
performance of KubeSAN; document all of these, and update the
testsuite to use them.

However, in testing this, the kcli cluster based on Fedora 40 with
lvm2 v2.03.25 consistently encountered lock corruption when trying a
full 64M io_memory_size; cutting things back to 32M (still more than
the default of 8M) let the tests pass.

Signed-off-by: Eric Blake <eblake@redhat.com>

tests/lib/deploy-helpers.sh
@@ -182,7 +182,17 @@ __setup_nbd_storage() {
 
     for node_index in "${NODE_INDICES[@]}"; do
         __${deploy_tool}_ssh "${NODES[node_index]}" "
-            sudo sed -i 's|# use_lvmlockd = 0|use_lvmlockd = 1|' /etc/lvm/lvm.conf
+            sudo sed -i '
+                 s|# validate_metadata = \\\"full\\\"|validate_metadata = \\\"none\\\"|
+                 s|# multipath_component_detection = 1|multipath_component_detection = 0|
+                 s|# md_component_detection = 1|md_component_detection = 0|
+                 s|# backup = 1|backup = 0|
+                 s|# archive = 1|archive = 0|
+                 s|# use_lvmlockd = 0|use_lvmlockd = 1|
+                 s|# thin_check_options = \\[.*\\]|thin_check_options = [ \\\"-q\\\", \\\"--clear-needs-check-flag\\\", \\\"--skip-mappings\\\" \\]|
+                 s|# io_memory_size = 8192|io_memory_size = 32768|
+                 s|# reserved_memory = 8192|reserved_memory = 0|
+            ' /etc/lvm/lvm.conf
             sudo sed -i 's|# host_id = 0|host_id = $((node_index + 1))|' /etc/lvm/lvmlocal.conf
 
             # TODO set up watchdog

commit 119a00c5ae341465b2230814e57e86bed2be5289

manager: Document more metadatasize tweaks
We just recently recommended tweaking lvm.conf io_memory_size (units
of 1K) from 8k to 64k (commit bf91e85e); this number was based on
supporting enough metadata to create 10,000 LVs in a single VG.  But
it only matters if the vgcreate also requests a larger-than-default
metadata area, which we have not yet documented.  And, contrary to the
reason stated in the earlier commit, I finally determined that the
reason I was getting testsuite failures at 64k but not at 32k was
because the KubeSAN node pod was getting OOMKilled when that much
memory was allocated, which in turn could leave lvmlockd in unusual
states.

What we missed documenting: the vgcreate metadatasize defaults to 1M,
and must contain a minimum of 2 copies of metadata (ie. the default
maximum in-memory metadata size is 512k, much smaller than the 8M
available with the default io_memory_size).  I just measured that the
creation of 1000 Thin PVCs (each one requiring a thin pool and thin
LV) on a VG with non-default metadatasize, and it consumed 1.8M
(compare `vgs -o +vg_mda_free` before and after).  That is, with a
default-sized metadatasize of 1M, my test would have been unable to
create more than about 300 PVCs before it runs out of metadata space,
even if there were still plenty of extents left.  The reason Dave
Teigland had recommended io_memory_size of 64k was because he was also
recommending vgcreate with --metadatasize 128M to ensure he could do
scale testing well past 10,000 volumes (take my 1000 PVC test that
measured at 1.8M, scale it by an order of magnitude to 10,000 PVCs at
18M, and triple the amount of metadata consumed by attaching more tags
to each LV - that puts you at an in-memory metadata use of 54M for one
copy; then anything more than double that means the circular buffer on
disk can preserve a bit more history should we need to reconstruct
when metadata changes go wrong).

In consulting with Dave over these numbers today, he said that
io_memory_size of 64k and --metadatasize 128M were for scaling tests;
he wasn't sure if actual clusters will ever regularly go over 5,000
PVCs, so he thought more moderate numbers of io_memory_size 32k and
--metadatadsize 64M might be friendlier to document.  Dave had one
other recommended tweak: for a SAN with 512-byte sectors, there is no
change, but for a SAN with 4k native sectors, chewing up 2000 sectors
requires 8M which no longer benefits from the atomicity of a kernel 2M
transparent huge page; performance improves if you limit yourself to
500 hosts and only 2M lockspace (something added in lvm 2.03.28).

For our kcli cluster, where we only have 2G in the VG to begin with,
we won't be creating so many volumes as to need to tweak things as
high as we recommend for a large cluster (after all, since each thin
pool requires 2 LVs, and each LV requires at least one 4M extent, you
can't even fit 256 thin pools in a 2G VG), so that should stay at
--metadatasize 1M.  But we can still bump up io_memory_size to 64k to
prove the new pod memory grants are large enough to avoid OOM even in
a cluster that sizes things large.

Fixes: bf91e85e ("docs: Mention more lvm.conf tunables that we recommend", v0.10.2)

deploy/kubernetes/manager/node.yaml
@@ -60,7 +60,7 @@ spec:
           resources:
             limits:
               cpu: 500m
-              memory: 128Mi
+              memory: 512Mi
             requests:
               cpu: 10m
               memory: 64Mi

tests/lib/deploy-helpers.sh
@@ -190,7 +190,9 @@ __setup_nbd_storage() {
                  s|# archive = 1|archive = 0|
                  s|# use_lvmlockd = 0|use_lvmlockd = 1|
                  s|# thin_check_options = \\[.*\\]|thin_check_options = [ \\\"-q\\\", \\\"--clear-needs-check-flag\\\", \\\"--skip-mappings\\\" \\]|
-                 s|# io_memory_size = 8192|io_memory_size = 32768|
+                 # This value intentionally higher than docs, for the
+                 # sake of proving we do not run out of memory
+                 s|# io_memory_size = 8192|io_memory_size = 65536|
                  s|# reserved_memory = 8192|reserved_memory = 0|
             ' /etc/lvm/lvm.conf
             sudo sed -i 's|# host_id = 0|host_id = $((node_index + 1))|' /etc/lvm/lvmlocal.conf

tests/lib/shared-vg.sh
@@ -2,8 +2,14 @@
 
 # usage __create_ksan_sharedvg <vgname> <device>
 __create_ksan_shared_vg() {
+    # In a production environment, with a much larger amount of storage
+    # to share, you really want --metadatasize to be at least twice
+    # the io_memory_size in /etc/lvm/lvm.conf, to allow for the maximum
+    # number of LVs to fit in that VG.  But for our test cluster, where
+    # the VG is 2G in size and can't even fit 256 thin pools, we can
+    # explicitly reuse VG's default of a 1M metadatasize.
     __${deploy_tool}_ssh "${NODES[0]}" "
-        sudo vgcreate --devicesfile "$1" --shared "$1" "$2"
+        sudo vgcreate --devicesfile "$1" --metadatasize 1M --shared "$1" "$2"
     "
 
     # The devices file setup is important: